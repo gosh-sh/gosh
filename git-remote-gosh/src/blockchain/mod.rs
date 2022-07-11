7428ab5954a3ac146ed8b1d07258e2fc26ba2c91
Vasily Selivanov <v.selivanov@gmail.com>
2022-07-11 11:29:10 +0300

    add some logging

--- git-remote-gosh/src/blockchain/mod.rs
+++ git-remote-gosh/src/blockchain/mod.rs
@@ -417,6 +417,7 @@ pub async fn get_head(context: &TonClient, address: &str) -> Result<String, Box<
     return Ok(result.head);
 }
 
+#[instrument(level = "debug", skip(context))]
 pub async fn load_messages_to(
     context: &TonClient,
     address: &str,
@@ -461,6 +462,8 @@ pub async fn load_messages_to(
     let nodes = result["data"]["blockchain"]["account"]["messages"]["edges"]
         .as_array()
         .unwrap();
+
+    log::debug!("Loaded {} message(s) to {}", nodes.len(), address);
     for message in nodes {
         let raw_msg: Message = serde_json::from_value(message["node"].clone()).unwrap();
         if raw_msg.status != 5 || raw_msg.bounced {
@@ -478,6 +481,7 @@ pub async fn load_messages_to(
         )
         .await?;
 
+        log::debug!("Decoded message `{}`", decoded.name);
         if decoded.name == "applyDiff" {
             let value = decoded.value.unwrap();
             let diff: Diff = serde_json::from_value(value["diff"].clone()).unwrap();
