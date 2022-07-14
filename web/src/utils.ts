ba34b978d7e786d3c0a7020888b14b460d1288db
rymka1989 <rymka1989@gmail.com>
2022-07-15 00:45:52 +0300

    web: repo list - search/paginate optimization

--- web/src/utils.ts
+++ web/src/utils.ts
@@ -1,9 +1,19 @@
-export const classNames = (...classes: any[]): string => classes.filter(Boolean).join(' ');
+export const classNames = (...classes: any[]): string =>
+    classes.filter(Boolean).join(' ');
 
-export const shortString = (data: string, start: number = 6, end: number = 6, delimiter: string = '...'): string => {
+export const shortString = (
+    data: string,
+    start: number = 6,
+    end: number = 6,
+    delimiter: string = '...'
+): string => {
     if (data.length <= start + end) return data;
 
     const left = data.substring(0, start);
     const right = data.substring(data.length - end);
     return `${left}${delimiter}${right}`;
-}
+};
+
+export const sleep = (ms: number = 0) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+};
