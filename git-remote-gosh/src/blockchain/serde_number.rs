45844a51ff6cb3456c8ee5455f825052fe5f2293
Andrew Kurochkin <andrew.k@gosh.sh>
2022-08-22 02:27:19 +0300

    Revert "fix u128 deser"
    This reverts commit 0efe2e981c298901129785afbaf429b6725b550f.

--- git-remote-gosh/src/blockchain/serde_number.rs
+++ git-remote-gosh/src/blockchain/serde_number.rs
@@ -48,35 +48,3 @@ impl<'de> Deserialize<'de> for Number {
         deserializer.deserialize_any(MyVisitor)
     }
 }
-
-pub mod uint {
-    use super::*;
-
-    pub fn deserialize<'de, D>(d: D) -> Result<u128, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        let string = d.deserialize_option(ton_sdk::json_helper::StringVisitor)?;
-
-        if "null" == string {
-            return Ok(0);
-        }
-
-        if !string.starts_with("0x") {
-            return Err(D::Error::custom(format!(
-                "Number parsing error: number must be prefixed with 0x ({})",
-                string
-            )));
-        }
-
-        u128::from_str_radix(&string[2..], 16)
-            .map_err(|err| D::Error::custom(format!("Error parsing number: {}", err)))
-    }
-
-    pub fn serialize<S>(value: &u64, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        serializer.serialize_str(&format!("0x{:x}", value))
-    }
-}
