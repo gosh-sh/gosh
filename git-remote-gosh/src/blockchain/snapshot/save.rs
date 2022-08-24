1d544146cfc3f5c6aa6ca14460e70b3b9fcc57eb
Andrew <andrew.kurochkin@outlook.com>
2022-08-24 18:41:08 +0300

    Feature/retry push initial snapshot (#61)
    * feature/retry-push-initial-snapshot
    
    * update messages
    
    * more explicit failure point
    
    Co-authored-by: Andrew Kurochkin <andrew.k@gosh.sh>

--- git-remote-gosh/src/blockchain/snapshot/save.rs
+++ git-remote-gosh/src/blockchain/snapshot/save.rs
@@ -11,6 +11,7 @@ use reqwest::multipart;
 use snapshot::Snapshot;
 
 const PUSH_DIFF_MAX_TRIES: i32 = 3;
+const PUSH_SNAPSHOT_MAX_TRIES: i32 = 3;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
@@ -197,7 +198,7 @@ pub async fn push_diff(
                 std::thread::sleep(std::time::Duration::from_secs(5));
             }
         };
-        result.map_err(|e| e.description().to_string())
+        result.map_err(|e| e.to_string())
     }));
 }
 
@@ -365,20 +366,33 @@ pub async fn push_initial_snapshot(
         content: "".to_string(),
         ipfs: None,
     };
+    let branch_name = branch_name.to_string();
+    let file_path = file_path.to_string();
     let wallet = user_wallet(context).await?;
     let params = serde_json::to_value(args)?;
     let es_client = context.es_client.clone();
     Ok(tokio::spawn(async move {
-        let result = call(
-            &es_client,
-            wallet,
-            "deployNewSnapshot",
-            Some(params),
-        )
-        .await
-        .map_err(|e| e.description().to_string())
-        .and_then(|e| Ok(()));
-        log::debug!("deployNewSnapshot result: {:?}", result);
+        let mut attempt = 0;
+        let result = loop {
+            attempt += 1;
+            let result = call(
+                &es_client,
+                wallet.clone(),
+                "deployNewSnapshot",
+                Some(params.clone()),
+            )
+            .await
+            .map_err(|e| e.to_string())
+            .and_then(|e| Ok(()));
+
+            if result.is_ok() || attempt > PUSH_SNAPSHOT_MAX_TRIES {
+                break result;
+            } else {
+                log::debug!("inner_push_snapshot error <branch: {branch_name}, path: {file_path}>");
+                std::thread::sleep(std::time::Duration::from_secs(5));
+            }
+        };
+        log::debug!("deployNewSnapshot <branch: {branch_name}, path: {file_path}> result: {:?}", result);
         result
     }))
 }
