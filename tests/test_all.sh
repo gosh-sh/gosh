178a00598920348ae3343a181673a288d237ac62
Oxana Selivanova <39991771+Oxydixi@users.noreply.github.com>
2022-08-27 22:44:03 +0300

    Integration tests (#43)
    * WIP
    
    * added set-vars and test 02-create_branch
    
    * updated tests
    
    * added test 03
    
    * added test 04
    
    * feature: include tests
    
    Co-authored-by: My name <foo@bar.com>
    Co-authored-by: sostrovskyi <sostrovskyi>

---
+++ tests/test_all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e 
+set -o pipefail
+
+. set-vars.sh
+. 01-clone_empty_repo.sh
+. 02-create_branch.sh
+. 03-push_multiple_updates_in_a_single_commit.sh
+. 04-push_parallel_diffs_in_multiple_commite.sh
