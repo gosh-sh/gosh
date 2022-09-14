9ad144ad75c38be021e039761c08923942ded6d3
sostrovskyi <sostrovskyi>
2022-09-14 13:43:00 +0100

    tests: disable until infra fix

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -2,9 +2,9 @@
 set -e 
 set -o pipefail
 
-. set-vars.sh
-./01-clone_empty_repo.sh
-./02-create_branch.sh
-./03-push_multiple_updates_in_a_single_commit.sh
+#. set-vars.sh
+#./01-clone_empty_repo.sh
+#./02-create_branch.sh
+#./03-push_multiple_updates_in_a_single_commit.sh
 #./04-push_parallel_diffs_in_multiple_commite.sh
 #./05-bin_test.sh
