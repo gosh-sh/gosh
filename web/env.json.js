786c65ca100a89dcb2cac08b01f8df76a855727d
rymka1989 <rymka1989@gmail.com>
2022-09-14 11:27:42 +0300

    web: update public envs generator

--- web/env.json.js
+++ web/env.json.js
@@ -4,8 +4,7 @@ writeFileSync(
     './public/envs.json',
     JSON.stringify({
         network: process.env.REACT_APP_GOSH_NETWORK,
-        gosh: process.env.REACT_APP_GOSH_ADDR,
-        creator: process.env.REACT_APP_CREATOR_ADDR,
+        root: process.env.REACT_APP_GOSH_ROOTADDR,
         ipfs: process.env.REACT_APP_IPFS,
     }),
 )
