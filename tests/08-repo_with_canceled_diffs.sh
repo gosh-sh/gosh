4a49f9540fcf62b38930fa2286209829622a1ad8
My name <foo@bar.com>
2022-10-10 22:37:29 +0300

    added 08 test

---
+++ tests/08-repo_with_canceled_diffs.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+# 1. create repo
+# 2. clone repo into repo8-clone1
+# 3. new file in repo8-clone1
+# 4. push changes
+# 5. clone repo in repo8-clone2
+# 6. rewrite file in repo8-clone1
+# 7. push changes
+# 8. new file in repo8-clone2
+# 9. push changes
+# 10. clone repo in repo8-clone3
+# 11. comparing repositories repo8-clone1 and repo8-clone3 (similar)
+
+set -e 
+set -o pipefail
+. ./util.sh
+
+#1
+REPO_NAME=repo8
+
+[ -d $REPO_NAME"-clone1" ] && rm -rf $REPO_NAME"-clone1"
+[ -d $REPO_NAME"-clone2" ] && rm -rf $REPO_NAME"-clone2"
+[ -d $REPO_NAME"-clone3" ] && rm -rf $REPO_NAME"-clone3"
+
+tonos-cli call --abi $WALLET_ABI --sign $WALLET_KEYS $WALLET_ADDR deployRepository "{\"nameRepo\":\"$REPO_NAME\"}" || exit 1
+REPO_ADDR=$(tonos-cli -j run $GOSH_ROOT_ADDR getAddrRepository "{\"name\":\"$REPO_NAME\",\"dao\":\"$DAO1_NAME\"}" --abi $GOSH_ABI | sed -n '/value0/ p' | cut -d'"' -f 4)
+
+echo "***** awaiting repo deploy *****"
+wait_account_active $REPO_ADDR
+sleep 60
+
+# 2
+echo "***** cloning repo *****"
+git clone gosh::$NETWORK://$GOSH_ROOT_ADDR/$DAO1_NAME/$REPO_NAME $REPO_NAME"-clone1"
+
+cd $REPO_NAME"-clone1"
+
+# config git client
+git config user.email "foo@bar.com"
+git config user.name "My name"
+
+CHANGE=$(date +%s)
+BRANCH_NAME=branch-$CHANGE
+
+# create branch
+git checkout -b $BRANCH_NAME
+
+# 3
+FILE1=a.txt
+echo "Hello a! its $CHANGE now" > $FILE1
+
+# 4
+git add .
+git commit -m "added-$FILE1-now-$CHANGE"
+
+echo "***** awaiting push $FILE1 into $BRANCH_NAME *****"
+git push -u origin $BRANCH_NAME
+
+echo "***** awaiting set commit into $BRANCH_NAME *****"
+wait_set_commit $REPO_ADDR $BRANCH_NAME
+sleep 120
+
+cd ..
+
+# 5
+git clone gosh::$NETWORK://$GOSH_ROOT_ADDR/$DAO1_NAME/$REPO_NAME $REPO_NAME"-clone2"
+
+cd $REPO_NAME"-clone1"
+
+# 6
+echo "Hello again A! its $CHANGE now" > $FILE1
+
+# 7
+git add .
+git commit -m "rewrite-$FILE1-now-$CHANGE"
+
+echo "***** awaiting push $FILE1 into $BRANCH_NAME *****"
+git push -u origin $BRANCH_NAME
+
+echo "***** awaiting set commit into $BRANCH_NAME *****"
+wait_set_commit $REPO_ADDR $BRANCH_NAME
+sleep 120
+
+# 8
+cd ../$REPO_NAME"-clone2"
+
+# config git client
+git config user.email "foo@bar.com"
+git config user.name "My name"
+
+FILE2=b.txt
+echo "Hello b! its $CHANGE now" > $FILE2
+
+# 9
+git add .
+git commit -m "added-$FILE2-now-$CHANGE"
+
+echo "***** push $FILE2 into $BRANCH_NAME *****"
+git push -u origin $BRANCH_NAME
+
+sleep 120
+
+cd ..
+
+# 10
+echo "***** cloning repo *****"
+git clone gosh::$NETWORK://$GOSH_ROOT_ADDR/$DAO1_NAME/$REPO_NAME $REPO_NAME"-clone3"
+
+# 11
+echo "***** comparing repositories *****"
+DIFF_STATUS=1
+if  diff --brief --recursive $REPO_NAME"-clone1" $REPO_NAME"-clone3" --exclude ".git"; then
+    DIFF_STATUS=0
+fi
+
+exit $DIFF_STATUS
