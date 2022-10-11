2b0a422db6c85aff7d37777cf9b863b3a6ea6b3d
sostrovskyi <sostrovskyi>
2022-10-11 22:05:41 +0100

    append tests

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -7,7 +7,7 @@ set -o pipefail
 ./02-create_branch.sh
 ./03-push_multiple_updates_in_a_single_commit.sh
 ./04-push_parallel_diffs_in_multiple_commite.sh
-#./05-bin_test.sh
+./05-bin_test.sh
 ./06-sequential_merges.sh
-#./07-push_to_empty_main.sh
-#./08-repo_with_canceled_diffs.sh
+./07-push_to_empty_main.sh
+./08-repo_with_canceled_diffs.sh
