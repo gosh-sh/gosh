793717dadeed5b75bd8fb2fd8821e8a8562c3fb8
rymka1989 <rymka1989@gmail.com>
2022-10-20 16:17:12 +0300

    web: create branch from empty branch error

--- react-gosh/src/gosh/0.11.0/adapter.ts
+++ react-gosh/src/gosh/0.11.0/adapter.ts
@@ -823,6 +823,9 @@ class GoshRepositoryAdapter implements IGoshRepositoryAdapter {
 
         // Get branch details and check name
         const fromBranch = await this.getBranch(from)
+        if (fromBranch.commit.name === ZERO_COMMIT) {
+            throw new GoshError(EGoshError.BRANCH_CREATE_EMPTY)
+        }
         if (fromBranch.name === name) return
 
         // Get `from` branch tree items and collect all blobs
