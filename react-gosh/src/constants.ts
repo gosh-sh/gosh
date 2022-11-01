7619d157d3b1145ed4cdec032ada31d7428ad472
rymka1989 <rymka1989@gmail.com>
2022-11-01 14:13:13 +0300

    web: update max parallel constants

--- react-gosh/src/constants.ts
+++ react-gosh/src/constants.ts
@@ -1,7 +1,7 @@
 export const ZERO_COMMIT = '0000000000000000000000000000000000000000'
 export const MAX_ONCHAIN_SIZE = 64512
-export const MAX_PARALLEL_READ = 20
-export const MAX_PARALLEL_WRITE = 100
+export const MAX_PARALLEL_READ = 600
+export const MAX_PARALLEL_WRITE = 200
 
 export const EventTypes: { [key: number]: string } = {
     1: 'Pull request',
