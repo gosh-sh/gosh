38468e908a2a17fbeaa99aa9dfa1c65d766dfc6a
Oxana Selivanova <39991771+Oxydixi@users.noreply.github.com>
2022-11-03 12:59:23 +0300

    Added test for submodules (#178)
    * update tests
    
    * added 11 test
    
    Co-authored-by: Oxana Selivanova <oxana.s@gosh.sh>

--- tests/set-vars.sh
+++ tests/set-vars.sh
@@ -8,42 +8,42 @@ echo "NETWORK=$NETWORK" > env.env
 
 tonos-cli config --url $NETWORK
 
-# Should exist root and gosh root
+# Should exist VersionConrtroler and SystemContract contracts
 
-export GOSH_ROOT_ADDR=`cat ../contracts/gosh/SystemContract.addr`
-echo "export GOSH_ROOT_ADDR=$GOSH_ROOT_ADDR" >> env.env
+export SYSTEM_CONTRACT_ADDR=`cat ../contracts/gosh/SystemContract.addr`
+echo "export SYSTEM_CONTRACT_ADDR=$SYSTEM_CONTRACT_ADDR" >> env.env
 
-export GOSH_ABI=../contracts/gosh/systemcontract.abi.json
+export SYSTEM_CONTRACT_ABI=../contracts/gosh/systemcontract.abi.json
 USER_PROFILE_ABI=../contracts/gosh/profile.abi.json
-echo "export GOSH_ABI=$GOSH_ABI" >> env.env
+echo "export SYSTEM_CONTRACT_ABI=$SYSTEM_CONTRACT_ABI" >> env.env
 export REPO_ABI=../contracts/gosh/repository.abi.json
 echo "export REPO_ABI=$REPO_ABI" >> env.env
-DAO1_ABI=../contracts/gosh/goshdao.abi.json
+DAO_ABI=../contracts/gosh/goshdao.abi.json
 
 # generate user keys
 SEED=`tonos-cli genphrase | grep -o '".*"' | tr -d '"'`
-DAO1_KEYS=test.keys.json
-tonos-cli getkeypair -o $DAO1_KEYS -p "$SEED"
-DAO1_PUBKEY=$(cat $DAO1_KEYS | sed -n '/public/ s/.*\([[:xdigit:]]\{64\}\).*/0x\1/p')
+DAO_KEYS=test.keys.json
+tonos-cli getkeypair -o $DAO_KEYS -p "$SEED"
+DAO_PUBKEY=$(cat $DAO_KEYS | sed -n '/public/ s/.*\([[:xdigit:]]\{64\}\).*/0x\1/p')
 
 # *create Profile
-USER_PROFILE_NAME="@user201"
-tonos-cli call --abi $GOSH_ABI $GOSH_ROOT_ADDR deployProfile "{\"pubkey\":\"$DAO1_PUBKEY\",\"name\":\"$USER_PROFILE_NAME\"}"
-USER_PROFILE_ADDR=$(tonos-cli -j run $GOSH_ROOT_ADDR getProfileAddr "{\"name\":\"$USER_PROFILE_NAME\"}" --abi $GOSH_ABI | sed -n '/value0/ p' | cut -d'"' -f 4)
+USER_PROFILE_NAME="@user106"
+tonos-cli call --abi $SYSTEM_CONTRACT_ABI $SYSTEM_CONTRACT_ADDR deployProfile "{\"pubkey\":\"$DAO_PUBKEY\",\"name\":\"$USER_PROFILE_NAME\"}"
+USER_PROFILE_ADDR=$(tonos-cli -j run $SYSTEM_CONTRACT_ADDR getProfileAddr "{\"name\":\"$USER_PROFILE_NAME\"}" --abi $SYSTEM_CONTRACT_ABI | sed -n '/value0/ p' | cut -d'"' -f 4)
 
 # *deploy DAO
-export DAO1_NAME=dao-100
-echo "export DAO1_NAME=$DAO1_NAME" >> env.env
-tonos-cli call --abi $USER_PROFILE_ABI $USER_PROFILE_ADDR --sign $DAO1_KEYS deployDao \
-  "{\"goshroot\":\"$GOSH_ROOT_ADDR\", \"name\":\"$DAO1_NAME\", \"previous\":null, \"pubmem\":[\"$USER_PROFILE_ADDR\"]}"
-DAO1_ADDR=$(tonos-cli -j run $GOSH_ROOT_ADDR getAddrDao "{\"name\":\"$DAO1_NAME\"}" --abi $GOSH_ABI | sed -n '/value0/ p' | cut -d'"' -f 4)
+export DAO_NAME=dao-106
+echo "export DAO_NAME=$DAO_NAME" >> env.env
+tonos-cli call --abi $USER_PROFILE_ABI $USER_PROFILE_ADDR --sign $DAO_KEYS deployDao \
+  "{\"goshroot\":\"$SYSTEM_CONTRACT_ADDR\", \"name\":\"$DAO_NAME\", \"previous\":null, \"pubmem\":[\"$USER_PROFILE_ADDR\"]}"
+DAO_ADDR=$(tonos-cli -j run $SYSTEM_CONTRACT_ADDR getAddrDao "{\"name\":\"$DAO_NAME\"}" --abi $SYSTEM_CONTRACT_ABI | sed -n '/value0/ p' | cut -d'"' -f 4)
 
 # user keys
-export WALLET_KEYS=$DAO1_KEYS
+export WALLET_KEYS=$DAO_KEYS
 echo "export WALLET_KEYS=$WALLET_KEYS" >> env.env
 WALLET_PUBKEY=$(cat $WALLET_KEYS | sed -n '/public/ s/.*\([[:xdigit:]]\{64\}\).*/0x\1/p')
 
-WALLET_ADDR=$(tonos-cli -j run $DAO1_ADDR getAddrWallet "{\"pubaddr\":\"$USER_PROFILE_ADDR\",\"index\":0}" --abi $DAO1_ABI | sed -n '/value0/ p' | cut -d'"' -f 4)
+WALLET_ADDR=$(tonos-cli -j run $DAO_ADDR getAddrWallet "{\"pubaddr\":\"$USER_PROFILE_ADDR\",\"index\":0}" --abi $DAO_ABI | sed -n '/value0/ p' | cut -d'"' -f 4)
 export WALLET_ADDR
 echo "export WALLET_ADDR=$WALLET_ADDR" >> env.env
 export WALLET_ABI=../contracts/gosh/goshwallet.abi.json
@@ -79,14 +79,14 @@ tee $USER_CONFIG <<EOF
 EOF
 
 echo ===============================================
-echo "  Gosh root:" $GOSH_ROOT_ADDR
+echo "System contract:" $SYSTEM_CONTRACT_ADDR
 echo ================ USER PROFILE =================
 echo "   Profile name:" $USER_PROFILE_NAME
 echo "Profile address:" $USER_PROFILE_ADDR
 echo ===================== DAO =====================
-echo "   DAO name:" $DAO1_NAME
-echo "DAO address:" $DAO1_ADDR
-echo "   DAO keys:" $(cat $DAO1_KEYS)
+echo "   DAO name:" $DAO_NAME
+echo "DAO address:" $DAO_ADDR
+echo "   DAO keys:" $(cat $DAO_KEYS)
 echo ==================== WALLET ===================
 echo "WALLET address:" $WALLET_ADDR
 echo "   WALLET keys:" $(cat $WALLET_KEYS)
