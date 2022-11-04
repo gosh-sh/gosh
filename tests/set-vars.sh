04a5159814e101914700820d92fc8062c0a06681
Oxana Selivanova <oxana.s@gosh.sh>
2022-11-04 11:27:13 +0300

    updated set-vars.sh

--- tests/set-vars.sh
+++ tests/set-vars.sh
@@ -35,7 +35,7 @@ USER_PROFILE_ADDR=$(tonos-cli -j run $SYSTEM_CONTRACT_ADDR getProfileAddr "{\"na
 export DAO_NAME=dao-106
 echo "export DAO_NAME=$DAO_NAME" >> env.env
 tonos-cli call --abi $USER_PROFILE_ABI $USER_PROFILE_ADDR --sign $DAO_KEYS deployDao \
-  "{\"goshroot\":\"$SYSTEM_CONTRACT_ADDR\", \"name\":\"$DAO_NAME\", \"previous\":null, \"pubmem\":[\"$USER_PROFILE_ADDR\"]}"
+  "{\"systemcontract\":\"$SYSTEM_CONTRACT_ADDR\", \"name\":\"$DAO_NAME\", \"pubmem\":[\"$USER_PROFILE_ADDR\"]}"
 DAO_ADDR=$(tonos-cli -j run $SYSTEM_CONTRACT_ADDR getAddrDao "{\"name\":\"$DAO_NAME\"}" --abi $SYSTEM_CONTRACT_ABI | sed -n '/value0/ p' | cut -d'"' -f 4)
 
 # user keys
