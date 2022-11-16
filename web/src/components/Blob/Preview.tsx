494f08d1380cdb4f956ddf938d1794c214b36623
rymka1989 <rymka1989@gmail.com>
2022-11-16 17:10:28 +0300

    web: highlight.js for blob view/preview

--- web/src/components/Blob/Preview.tsx
+++ web/src/components/Blob/Preview.tsx
@@ -1,56 +1,63 @@
-import Editor from '@monaco-editor/react'
 import ReactMarkdown from 'react-markdown'
+import hljs from 'highlight.js'
 import { classNames } from 'react-gosh'
 import { Buffer } from 'buffer'
 
 type TBlobPreviewProps = {
-    language?: string
+    filename?: string
     value?: string | Buffer
     className?: string
 }
 
 const BlobPreview = (props: TBlobPreviewProps) => {
-    const { language, value, className } = props
+    const { filename = '', value = '', className } = props
 
-    if (Buffer.isBuffer(value))
+    const createHighlightedCodeBlock = (content: string, language?: string[]) => {
+        let lineNumber = 0
+        const highlightedContent = hljs.highlightAuto(content, language).value
+
+        /* Highlight.js wraps comment blocks inside <span class="hljs-comment"></span>.
+           However, when the multi-line comment block is broken down into diffirent
+           table rows, only the first row, which is appended by the <span> tag, is
+           highlighted. The following code fixes it by appending <span> to each line
+           of the comment block. */
+        const commentPattern = /<span class="hljs-comment">(.|\n)*?<\/span>/g
+        const adaptedHighlightedContent = highlightedContent.replace(
+            commentPattern,
+            (data) => {
+                return data.replace(/\r?\n/g, () => {
+                    return '\n<span class="hljs-comment">'
+                })
+            },
+        )
+
+        const contentTable = adaptedHighlightedContent
+            .split(/\r?\n/)
+            .map((lineContent) => {
+                return `<tr>
+                    <td class='line-number' data-pseudo-content=${++lineNumber}></td>
+                    <td>${lineContent}</td>
+                  </tr>`
+            })
+            .join('')
+
+        return `<pre><code><table class='code-table'>${contentTable}</table></code></pre>`
+    }
+
+    if (Buffer.isBuffer(value)) {
         return <p className="text-gray-606060 p-3 text-sm">Binary data not shown</p>
-    if (language === 'markdown')
+    }
+    if (filename.split('.').splice(-1)[0] === 'md') {
         return (
             <div className={classNames('markdown-body px-4 py-4', className)}>
                 <ReactMarkdown>{value || ''}</ReactMarkdown>
             </div>
         )
+    }
     return (
-        <Editor
-            wrapperProps={{
-                className,
-            }}
-            language={language}
-            value={value}
-            options={{
-                readOnly: true,
-                renderLineHighlight: 'none',
-                contextmenu: false,
-                automaticLayout: true,
-                scrollBeyondLastLine: false,
-                minimap: {
-                    enabled: false,
-                },
-                // scrollbar: {
-                //     vertical: 'hidden',
-                //     verticalScrollbarSize: 0,
-                //     handleMouseWheel: false,
-                // },
-            }}
-            onMount={(editor) => {
-                // Set diff editor dom element calculated real height
-                editor.onDidContentSizeChange(() => {
-                    const node = editor.getDomNode()
-                    let height = editor.getContentHeight()
-                    if (height > 1024) height = 1024
-                    if (node) node.style.height = `${height}px`
-                })
-            }}
+        <div
+            className={classNames('text-sm', className)}
+            dangerouslySetInnerHTML={{ __html: createHighlightedCodeBlock(value) }}
         />
     )
 }
