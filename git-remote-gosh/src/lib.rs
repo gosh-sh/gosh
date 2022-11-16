5553a5cd5cc72edeba752ee946284a9e35e19819
Andrew K <andrew.k@gosh.sh>
2022-11-16 10:20:50 +0200

    seems like it fixes trace problems

--- git-remote-gosh/src/lib.rs
+++ git-remote-gosh/src/lib.rs
@@ -24,35 +24,14 @@ extern crate tracing;
 extern crate diffy;
 extern crate lru;
 
+pub extern crate anyhow;
+
 pub mod abi;
 pub mod blockchain;
 pub mod config;
-pub(crate) mod git_helper;
+pub mod git_helper;
 pub mod ipfs;
-pub(crate) mod logger;
+pub mod logger;
 pub mod utilities;
 
-use logger::set_log_verbosity;
-use opentelemetry::global::shutdown_tracer_provider;
-use std::env::args;
 
-#[instrument(level = "debug")]
-pub async fn run() -> anyhow::Result<()> {
-    set_log_verbosity(1);
-
-    {
-        let root = span!(tracing::Level::TRACE, "git-remote-helper");
-        let _enter = root.enter();
-        let config = config::Config::init()?;
-        let version = option_env!("GOSH_BUILD_VERSION").unwrap_or(env!("CARGO_PKG_VERSION"));
-        tracing::info!("git-remote-gosh v{version}");
-        eprintln!("git-remote-gosh v{version}");
-        let url = args().nth(2).ok_or(anyhow::anyhow!(
-            "Wrong args for git-remote call\nRequired: <name> <url>"
-        ))?;
-        crate::git_helper::run(config, &url).await?;
-    }
-
-    shutdown_tracer_provider();
-    Ok(())
-}
