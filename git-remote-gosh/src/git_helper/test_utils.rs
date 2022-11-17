c8797eb96e85cfe82d3f5d9a3ca4f59aa86b8b7a
SilkovAlexander <slexander.s@tonlabs.io>
2022-11-17 12:37:39 +0300

    Added tracing to tests

--- git-remote-gosh/src/git_helper/test_utils.rs
+++ git-remote-gosh/src/git_helper/test_utils.rs
@@ -1,6 +1,30 @@
 use git_object::bstr::ByteSlice;
 use git_repository::Repository;
 use std::{env, fs};
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::Once;
+use std::time::Duration;
+use opentelemetry::global::shutdown_tracer_provider;
+use tokio::time::sleep;
+use crate::logger::set_log_verbosity;
+
+static TEST_LOGGER: Once = Once::new();
+static CALL_COUNT: AtomicUsize = AtomicUsize::new(0);
+
+pub async fn init_logger() {
+    TEST_LOGGER.call_once(|| {
+        set_log_verbosity(5);
+    });
+    CALL_COUNT.fetch_add(1, Ordering::SeqCst);
+}
+
+pub async fn shutdown_logger() {
+    CALL_COUNT.fetch_sub(1, Ordering::SeqCst);
+    if CALL_COUNT.load(Ordering::SeqCst) == 0 {
+        sleep(Duration::from_secs(10)).await;
+        // shutdown_tracer_provider();
+    }
+}
 
 pub fn setup_repo(name: &str, script_path: &str) -> anyhow::Result<Repository> {
     eprintln!("Testing push: current_dir = {:?}", env::current_dir()?);
