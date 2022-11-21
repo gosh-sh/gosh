423feaf592c2766578aea7d149b02d3384dc3fba
Vasily Selivanov <gosh@evex.io>
2022-11-21 17:38:24 +0300

    remove TODO-comment

--- git-remote-gosh/src/git_helper/push/mod.rs
+++ git-remote-gosh/src/git_helper/push/mod.rs
@@ -322,7 +322,6 @@ where
 
         let mut statistics = PushBlobStatistics::new();
         let mut parallel_diffs_upload_support = ParallelDiffsUploadSupport::new(&latest_commit_id);
-        // TODO: Handle deleted fules
         // Note: These files will NOT appear in the list here
         let out = String::from_utf8(cmd_out.stdout)?;
         // 3. If branch needs to be created do so
