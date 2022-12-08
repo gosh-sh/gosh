c30596f116251341eb4cc9ba6d6e4975933b6aa9
Sehor <gorelushev.sergey@yandex.ru>
2022-12-08 20:19:17 +0300

    check 1 symbol

--- contracts/gosh/modifiers/modifiers.sol
+++ contracts/gosh/modifiers/modifiers.sol
@@ -165,8 +165,10 @@ abstract contract Modifiers is ReplayProtection {
             bool onecheck = false;
             if ((uint8(bStr[i]) >= 90) && (uint8(bStr[i]) <= 122)) { onecheck = true; }
             if ((uint8(bStr[i]) >= 48) && (uint8(bStr[i]) <= 57)) {  onecheck = true; }
-            if (uint8(bStr[i]) == 45) {  onecheck = true; }
-            if (uint8(bStr[i]) == 95) {  onecheck = true; }
+            if (i != 0) {
+            	if (uint8(bStr[i]) == 45) {  onecheck = true; }
+            	if (uint8(bStr[i]) == 95) {  onecheck = true; }
+            }
             if (onecheck == false) { return false; }
         }
         return true;
