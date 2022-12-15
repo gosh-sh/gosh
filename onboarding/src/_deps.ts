5450e40310a094de4bbe2f9c4a5edde4f3de52f8
Sergei Blinov <blinovsv@gmail.com>
2022-12-15 04:53:40 +0100

    onboarding: fmt

--- onboarding/src/_deps.ts
+++ onboarding/src/_deps.ts
@@ -1,4 +1,4 @@
+import * as _dotenv from 'https://deno.land/x/dotenv@v3.2.0/mod.ts'
 import * as _sleep from 'https://deno.land/x/sleep@v1.2.1/mod.ts'
 import * as _supabase from 'https://esm.sh/@supabase/supabase-js@2.1.3'
 import * as _eversdk from 'npm:@eversdk/core'
-import * as _dotenv from 'https://deno.land/x/dotenv@v3.2.0/mod.ts'
