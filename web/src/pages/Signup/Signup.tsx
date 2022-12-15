df8dc7beeec88a5edcfb846014f75b360ac52a47
rymka1989 <rymka1989@gmail.com>
2022-12-15 22:00:08 +0300

    [web] - github flow deny access message

--- web/src/pages/Signup/Signup.tsx
+++ web/src/pages/Signup/Signup.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useRecoilState } from 'recoil'
-import { Navigate } from 'react-router-dom'
+import { Navigate, useLocation, useNavigate } from 'react-router-dom'
 import { GoshError, useUser } from 'react-gosh'
 import Spinner from '../../components/Spinner'
 import { toast } from 'react-toastify'
@@ -15,6 +15,8 @@ import GoshSignupStart from './GoshSignupStart'
 import GoshSignupPhrase from './GoshSignupPhrase'
 
 const SignupPage = () => {
+    const location = useLocation()
+    const navigate = useNavigate()
     const { persist } = useUser()
     const [githubSession, setGithubSession] = useRecoilState(githubSessionAtom)
     const [step, setStep] = useRecoilState(signupStepAtom)
@@ -65,6 +67,18 @@ const SignupPage = () => {
             return !state ? { index: 1 } : state
         })
     }, [githubSession, setStep])
+
+    useEffect(() => {
+        const params = new URLSearchParams(location.search)
+        if (params.get('error')) {
+            const e = {
+                title: params.get('error'),
+                message: params.get('error_description'),
+            }
+            toast.error(<ToastError error={e} />)
+            navigate('/')
+        }
+    }, [location.search, navigate])
 
     if (persist.pin) return <Navigate to="/a/orgs" />
     return (
