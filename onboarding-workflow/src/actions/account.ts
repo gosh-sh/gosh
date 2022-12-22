33323ee0e62dc428bc1c5cd406efd2e8999864de
Sergei Blinov <blinovsv@gmail.com>
2022-12-22 11:57:48 +0100

    add retry logs

--- onboarding-workflow/src/actions/account.ts
+++ onboarding-workflow/src/actions/account.ts
@@ -12,11 +12,13 @@ export function waitForAccountActive(addr: string) {
         .backoff('fixed', 10000)
 
     return new Promise((resolve, reject) => {
+        job.on('retrying', (err) => {
+            console.log(`Retry: ${job.id}`, err.message)
+        })
         job.on('failed', (err) => {
-            console.error(`Dao is not created`, err.message)
+            console.log(`Dao is not created`, err.message)
             reject(err)
         })
-
         job.on('succeeded', (res) => {
             resolve(res)
         })
@@ -24,19 +26,29 @@ export function waitForAccountActive(addr: string) {
     })
 }
 
-export function waitForWalletAccess(wallet_addr: string): Promise<unknown> {
+export function waitForWalletAccess(
+    wallet_addr: string,
+    wallet_pubkey: string,
+): Promise<unknown> {
     // wait for access
     const job = checkWalletAccessProducer()
         .createJob({
             wallet_addr,
+            wallet_pubkey,
         })
         // deduplication
         .setId(wallet_addr)
         .retries(MAX_RETRIES)
         .backoff('fixed', 10000)
     return new Promise((resolve, reject) => {
+        job.on('retrying', (err) => {
+            console.log(`Retry: ${job.id}`, err.message)
+        })
         job.on('failed', (err) => {
-            console.error(`Wallet access hasn't being granted`, err.message)
+            console.log(
+                `Error: ${job.id} Wallet access hasn't being granted`,
+                err.message,
+            )
             reject(err)
         })
         job.on('succeeded', (res) => {
