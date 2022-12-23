6f89ea1a1db664c80456a2688276a4bb1835a325
Sergei Blinov <blinovsv@gmail.com>
2022-12-23 16:45:04 +0100

    update db types

--- onboarding-workflow/src/db/types.ts
+++ onboarding-workflow/src/db/types.ts
@@ -71,52 +71,29 @@ export interface Database {
                 Row: {
                     id: string
                     created_at: string
-                    updated_at: string | null
                     user_id: string
                     github_url: string
                     gosh_url: string
                     dao_bot: string | null
+                    updated_at: string | null
                 }
                 Insert: {
                     id?: string
                     created_at?: string
-                    updated_at?: string | null
                     user_id: string
                     github_url: string
                     gosh_url: string
                     dao_bot?: string | null
+                    updated_at?: string | null
                 }
                 Update: {
                     id?: string
                     created_at?: string
-                    updated_at?: string | null
                     user_id?: string
                     github_url?: string
                     gosh_url?: string
                     dao_bot?: string | null
-                }
-            }
-            github_users: {
-                Row: {
-                    id: string
-                    user_id: string
-                    github_user_id: string | null
-                    full_name: string | null
-                    email: Json
-                }
-                Insert: {
-                    id?: string
-                    user_id: string
-                    github_user_id?: string | null
-                    full_name?: string | null
-                    email: Json
-                }
-                Update: {
-                    id?: string
-                    user_id?: string
-                    github_user_id?: string | null
-                    full_name?: string | null
-                    email?: Json
+                    updated_at?: string | null
                 }
             }
             test: {
@@ -167,7 +144,110 @@ export interface Database {
             }
         }
         Views: {
-            [_ in never]: never
+            auth_users: {
+                Row: {
+                    instance_id: string | null
+                    id: string | null
+                    aud: string | null
+                    role: string | null
+                    email: string | null
+                    encrypted_password: string | null
+                    email_confirmed_at: string | null
+                    invited_at: string | null
+                    confirmation_token: string | null
+                    confirmation_sent_at: string | null
+                    recovery_token: string | null
+                    recovery_sent_at: string | null
+                    email_change_token_new: string | null
+                    email_change: string | null
+                    email_change_sent_at: string | null
+                    last_sign_in_at: string | null
+                    raw_app_meta_data: Json | null
+                    raw_user_meta_data: Json | null
+                    is_super_admin: boolean | null
+                    created_at: string | null
+                    updated_at: string | null
+                    phone: string | null
+                    phone_confirmed_at: string | null
+                    phone_change: string | null
+                    phone_change_token: string | null
+                    phone_change_sent_at: string | null
+                    confirmed_at: string | null
+                    email_change_token_current: string | null
+                    email_change_confirm_status: number | null
+                    banned_until: string | null
+                    reauthentication_token: string | null
+                    reauthentication_sent_at: string | null
+                }
+                Insert: {
+                    instance_id?: string | null
+                    id?: string | null
+                    aud?: string | null
+                    role?: string | null
+                    email?: string | null
+                    encrypted_password?: string | null
+                    email_confirmed_at?: string | null
+                    invited_at?: string | null
+                    confirmation_token?: string | null
+                    confirmation_sent_at?: string | null
+                    recovery_token?: string | null
+                    recovery_sent_at?: string | null
+                    email_change_token_new?: string | null
+                    email_change?: string | null
+                    email_change_sent_at?: string | null
+                    last_sign_in_at?: string | null
+                    raw_app_meta_data?: Json | null
+                    raw_user_meta_data?: Json | null
+                    is_super_admin?: boolean | null
+                    created_at?: string | null
+                    updated_at?: string | null
+                    phone?: string | null
+                    phone_confirmed_at?: string | null
+                    phone_change?: string | null
+                    phone_change_token?: string | null
+                    phone_change_sent_at?: string | null
+                    confirmed_at?: string | null
+                    email_change_token_current?: string | null
+                    email_change_confirm_status?: number | null
+                    banned_until?: string | null
+                    reauthentication_token?: string | null
+                    reauthentication_sent_at?: string | null
+                }
+                Update: {
+                    instance_id?: string | null
+                    id?: string | null
+                    aud?: string | null
+                    role?: string | null
+                    email?: string | null
+                    encrypted_password?: string | null
+                    email_confirmed_at?: string | null
+                    invited_at?: string | null
+                    confirmation_token?: string | null
+                    confirmation_sent_at?: string | null
+                    recovery_token?: string | null
+                    recovery_sent_at?: string | null
+                    email_change_token_new?: string | null
+                    email_change?: string | null
+                    email_change_sent_at?: string | null
+                    last_sign_in_at?: string | null
+                    raw_app_meta_data?: Json | null
+                    raw_user_meta_data?: Json | null
+                    is_super_admin?: boolean | null
+                    created_at?: string | null
+                    updated_at?: string | null
+                    phone?: string | null
+                    phone_confirmed_at?: string | null
+                    phone_change?: string | null
+                    phone_change_token?: string | null
+                    phone_change_sent_at?: string | null
+                    confirmed_at?: string | null
+                    email_change_token_current?: string | null
+                    email_change_confirm_status?: number | null
+                    banned_until?: string | null
+                    reauthentication_token?: string | null
+                    reauthentication_sent_at?: string | null
+                }
+            }
         }
         Functions: {
             [_ in never]: never
