542c3c5e4886f19b5c8c13ca35a16a301420a5db
Sergei Blinov <blinovsv@gmail.com>
2023-01-05 13:41:43 +0100

    disable DB events for create dao, use timeout

--- onboarding-workflow/src/triggers/create_dao.ts
+++ onboarding-workflow/src/triggers/create_dao.ts
@@ -1,23 +1,28 @@
 import { Mutex } from 'https://deno.land/x/semaphore@v1.1.2/mod.ts'
+import { sleep } from 'https://deno.land/x/sleep@v1.2.1/mod.ts'
 import { initDaoBot } from '../actions/dao_bot.ts'
 import { getDaoBotsForInit } from '../db/dao_bot.ts'
-import { getDb } from '../db/db.ts'
+// import { getDb } from '../db/db.ts'
 
 const mutex = new Mutex()
 
-getDb()
-    .channel('dao_bots_for_init')
-    .on(
-        'postgres_changes',
-        { event: '*', schema: 'public', table: 'dao_bot' },
-        (payload) => {
-            console.log('dao bots updated', payload)
-            initNewDaoBots()
-        },
-    )
-    .subscribe()
+// getDb()
+//     .channel('dao_bots_for_init')
+//     .on(
+//         'postgres_changes',
+//         { event: '*', schema: 'public', table: 'dao_bot' },
+//         (payload) => {
+//             console.log('dao bots updated', payload)
+//             initNewDaoBots()
+//         },
+//     )
+//     .subscribe()
 
-initNewDaoBots()
+while (true) {
+    await initNewDaoBots()
+    console.log('Sleep...')
+    await sleep(30)
+}
 
 async function initNewDaoBots() {
     const release = await mutex.acquire()
