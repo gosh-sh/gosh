218e002a8e27c3519f71de59664dac64a8335024
Sergei Blinov <blinovsv@gmail.com>
2023-01-16 07:47:11 +0100

    push-clone repos separated by size

---
+++ onboarding-workflow/src/actions/count_git_objects.ts
@@ -0,0 +1,74 @@
+import { getDb } from '../db/db.ts'
+import { getGithubWithDaoBot } from '../db/github.ts'
+import {
+    createMediumGoshRepoProducer,
+    createSmallGoshRepoProducer,
+} from '../queues/mod.ts'
+
+export async function countGitObjects(github_id: string) {
+    const github = await getGithubWithDaoBot(github_id)
+    console.log('count git objects for', github)
+
+    const workdir = `/tmp/github/${github_id}`
+    const gitdir = `${workdir}/repo`
+    await Deno.mkdir(workdir, { recursive: true })
+
+    const github_full_repo_url = `https://github.com${github.github_url}`
+
+    const git_clone = Deno.run({
+        cmd: ['git', 'clone', github_full_repo_url, gitdir],
+        cwd: workdir,
+    })
+    const clone_status = await git_clone.status()
+    if (!clone_status.success) {
+        console.log(`Can't clone`, github_id)
+        return
+    }
+
+    // count git objects
+    const count_git_objects = Deno.run({
+        cmd: ['git', 'rev-list', '--all', '--count', '--objects'],
+        cwd: gitdir,
+        stdout: 'piped',
+    })
+
+    if (!(await count_git_objects.status()).success) {
+        console.log(`Fail to get number of git objects`, github_id)
+        return
+    }
+
+    const out_str = new TextDecoder().decode(await count_git_objects.output()).trim()
+    const number_of_git_objects = parseInt(out_str, 10)
+
+    console.log(`Repo`, github_id, `contains`, number_of_git_objects)
+
+    await getDb()
+        .from('github')
+        .update({ objects: number_of_git_objects })
+        .eq('id', github.id)
+
+    // cleanup
+    console.log('About to remove dir', workdir)
+    await Deno.remove(workdir, { recursive: true })
+    console.log('Dir removed', github_id)
+
+    // TODO: more logs
+    let producer = undefined
+    if (number_of_git_objects < 1500) {
+        producer = createSmallGoshRepoProducer()
+    } else if (number_of_git_objects < 15000) {
+        producer = createMediumGoshRepoProducer()
+    } else {
+        producer = createSmallGoshRepoProducer()
+    }
+
+    console.log('Schedule upload repository', github.id)
+    producer!
+        .createJob({
+            github_id: github.id,
+        })
+        // deduplication
+        .retries(5)
+        .setId(github.id)
+        .save()
+}
