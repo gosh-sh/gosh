cee3a320b09f3a822bddb1e3c8b88f6b94ba2418
SilkovAlexander <slexander.s@tonlabs.io>
2023-01-26 16:10:02 +0300

    Separated errors in build.rs

--- git-remote-gosh/build.rs
+++ git-remote-gosh/build.rs
@@ -3,17 +3,17 @@ use std::io;
 use std::io::BufRead;
 
 fn get_version_from_solidity_source() -> Vec<String> {
-    if let Ok(contracts_path) = std::env::var("CONTRACTS_DIR") {
-        if let Ok(version_source) = File::open(contracts_path + "/systemcontract.sol") {
-            for line in io::BufReader::new(version_source).lines() {
-                if let Ok(line) = line {
-                    if line.contains("string constant version = ") {
-                        return vec![line.trim_start_matches(|c| c != '"').trim_end_matches(|c| c != '"').replace(['\"'], "").to_string()];
-                    }
-                }
+    let contracts_path = std::env::var("CONTRACTS_DIR").expect("Failed to get GOSH contracts directory. Specify it with CONTRACTS_DIR env variable.")  + "/systemcontract.sol";
+    let version_source = File::open(&contracts_path).expect(&format!("Failed to open file: {}", &contracts_path));
+
+    for line in io::BufReader::new(version_source).lines() {
+        if let Ok(line) = line {
+            if line.contains("string constant version = ") {
+                return vec![line.trim_start_matches(|c| c != '"').trim_end_matches(|c| c != '"').replace(['\"'], "").to_string()];
             }
         }
     }
+
     panic!("Failed to load contract version from the sol file!");
 }
 
