e0a65d81c7ee1b0f0b57f2ea6bc840497c57a965
SilkovAlexander <slexander.s@tonlabs.io>
2023-02-02 17:35:40 +0300

    Moved clap to main

--- git-remote-gosh/src/bin/main.rs
+++ git-remote-gosh/src/bin/main.rs
@@ -1,11 +1,11 @@
+use clap::{Arg, Command};
 use git_remote_gosh::anyhow;
+use git_remote_gosh::git_helper::supported_contract_versions;
 use git_remote_gosh::logger::set_log_verbosity;
 use opentelemetry::global::shutdown_tracer_provider;
 use std::process::ExitCode;
 use std::time::Duration;
 use tokio::time::sleep;
-
-mod args;
 
 fn shutdown(result: anyhow::Result<()>) -> ExitCode {
     let exit_code = match result {
@@ -44,7 +44,33 @@ async fn main_internal() -> anyhow::Result<()> {
     let version = option_env!("GOSH_BUILD_VERSION").unwrap_or(env!("CARGO_PKG_VERSION"));
     tracing::info!("git-remote-gosh v{version}");
     eprintln!("git-remote-gosh v{version}");
-    let url = args::prepare_and_parse_args()?;
-    git_remote_gosh::git_helper::run(config, &url).await?;
+    let matches = Command::new("git-remote-gosh")
+        .about("GOSH network helper for git")
+        .version(option_env!("GOSH_BUILD_VERSION").unwrap_or(env!("CARGO_PKG_VERSION")))
+        .arg(Arg::new("name"))
+        .arg(Arg::new("url"))
+        .subcommand(
+            Command::new("supported_contract_versions")
+                .about("Get list of supported contract versions"),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("supported_contract_versions", _)) => {
+            println!(
+                "Supported contract versions: {:?}",
+                supported_contract_versions()
+            );
+        }
+        _ => {
+            let url = matches
+                .get_one::<String>("url")
+                .map(|s| s.to_string())
+                .ok_or(anyhow::anyhow!(
+                    "Wrong args for git-remote call\nRequired: <name> <url>"
+                ))?;
+            git_remote_gosh::git_helper::run(config, &url).await?;
+        }
+    }
     Ok(())
 }
