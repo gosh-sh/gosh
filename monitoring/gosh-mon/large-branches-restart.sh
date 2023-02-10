c6fab3568677903957831bccded269648aaccbcc
Skydev0h <alexhacker64@gmail.com>
2023-02-10 13:25:10 +0200

    Skip invalid rels, fix screenshots, concise config

--- monitoring/gosh-mon/large-branches-restart.sh
+++ monitoring/gosh-mon/large-branches-restart.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "$0")" || exit
 
 docker restart \
-  gosh-mon-next-rem-la-rd \
-  gosh-mon-next-rem-la-wr \
   gosh-mon-rem-la-rd \
   gosh-mon-rem-la-wr
+#  gosh-mon-next-rem-la-rd \
+#  gosh-mon-next-rem-la-wr
