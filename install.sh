7ffd761f17c740ad1e3a06384aec32e4ab7ed6cb
sostrovskyi <sostrovskyi>
2023-02-14 23:31:48 +0000

    dispatcher ci

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 REPO_OWNER=gosh-sh
 REPO=gosh
 RELEASE=rc-3.0.10
 SUPPORTED_CONTRACTS_VERSIONS=("1_0_0")
 
-# Define variables.
+# Check OS and architecture
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     if [[ `uname -m` == "x86_64" ]]; then
         TAR="git-remote-gosh-linux-amd64.tar"
@@ -26,14 +26,14 @@ else
 fi
 
 TAG=$(echo $RELEASE | tr -d 'rc-')
-EXECUTABLE_DIR=/usr/local/bin
-
 GH_API="https://api.github.com"
 GH_REPO="$GH_API/repos/${REPO_OWNER}/${REPO}"
 GH_TAGS="$GH_REPO/releases/tags/$TAG"
 
 # dir
-cd $EXECUTABLE_DIR
+
+mkdir -p $HOME/.gosh
+echo git-remote-gosh_v${SUPPORTED_CONTRACTS_VERSIONS[0]} > $HOME/.gosh/dispatcher.ini
 
 # Download release
 echo ""
@@ -47,32 +47,18 @@ response=$(curl -s $GH_TAGS)
 eval $(echo "$response" | grep -C3 "name.:.\+$TAR" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
 [ "$id" ] || { echo "Error: Failed to get asset id, response: $response" | awk 'length($0)<100' >&2; exit 1; }
 
-sudo wget --content-disposition --no-cookie --header "Accept: application/octet-stream" "$GH_REPO/releases/assets/$id" --show-progress
+wget --content-disposition --no-cookie -q --header "Accept: application/octet-stream" "$GH_REPO/releases/assets/$id" --show-progress
 
 # unpack
-sudo tar -xf $TAR
-sudo rm -f $TAR
+tar -xf $TAR
+rm -f $TAR
+########## DELETE THIS
+cp -r linux-amd64/git-remote-gosh ./
+cp -r git-remote-gosh_v${SUPPORTED_CONTRACTS_VERSIONS[0]} ./
 
-# make executable
-sudo chmod +x git-remote-gosh
-sudo chmod +x git-remote-gosh_v${SUPPORTED_CONTRACTS_VERSIONS[0]}
+# make executable 
+chmod +x git-remote-gosh
+chmod +x git-remote-gosh_v${SUPPORTED_CONTRACTS_VERSIONS[0]}
 
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  mkdir -p /home/$USER/.gosh/
-  echo git-remote-gosh_v${SUPPORTED_CONTRACTS_VERSIONS[0]} > /home/$USER/.gosh/dispatcher.ini
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  mkdir -p /Users/$USER/.gosh/
-  echo git-remote-gosh_v${SUPPORTED_CONTRACTS_VERSIONS[0]} > /Users/$USER/.gosh/dispatcher.ini
-else
-  echo "Only \"MacOS\" and \"Linux\" are supported - not \"$OSTYPE\""
-  exit 1;
-fi
-
-echo ""
-# smoke test executable installation
-if ! [ -f $EXECUTABLE_DIR/git-remote-gosh ]; then
-  echo "git-remote-gosh setup failed"
-  exit 1
-fi
-
-echo "$EXECUTABLE download successful"
+cp git-remote-gosh $HOME/.gosh/
+cp git-remote-gosh_v${SUPPORTED_CONTRACTS_VERSIONS[0]} $HOME/.gosh/
