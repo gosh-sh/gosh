132979bca0b7ad1eb21fa62a97742e3e69d04899
Oleksandr <alexhacker64@gmail.com>
2023-02-17 11:53:09 +0200

    Skip invalid rels, fix screenshots, concise config (#373)

--- monitoring/gosh-mon/next-restart.sh
+++ monitoring/gosh-mon/next-restart.sh
@@ -2,12 +2,12 @@
 
 cd "$(dirname "$0")" || exit
 
-docker restart \
-  gosh-mon-next-app-la-rd \
-  gosh-mon-next-app-la-wr \
-  gosh-mon-next-app-sm-rd \
-  gosh-mon-next-app-sm-wr \
-  gosh-mon-next-rem-la-rd \
-  gosh-mon-next-rem-la-wr \
-  gosh-mon-next-rem-sm-rd \
-  gosh-mon-next-rem-sm-wr
+# docker restart \
+#   gosh-mon-next-app-la-rd \
+#   gosh-mon-next-app-la-wr \
+#   gosh-mon-next-app-sm-rd \
+#   gosh-mon-next-app-sm-wr \
+#   gosh-mon-next-rem-la-rd \
+#   gosh-mon-next-rem-la-wr \
+#   gosh-mon-next-rem-sm-rd \
+#   gosh-mon-next-rem-sm-wr
