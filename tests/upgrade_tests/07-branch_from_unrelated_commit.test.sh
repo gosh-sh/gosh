02c256248c43b7d6e1dcc77335152e4dc1f0a012
SilkovAlexander <slexander.s@tonlabs.io>
2023-02-20 14:59:12 +0300

    Fixed test 07

--- tests/upgrade_tests/07-branch_from_unrelated_commit.test.sh
+++ tests/upgrade_tests/07-branch_from_unrelated_commit.test.sh
@@ -12,9 +12,11 @@ fi
 
 REPO_NAME=upgrade_repo07
 DAO_NAME="dao-upgrade-test07_$(date +%s)"
+NEW_REPO_PATH=upgrade_repo07_v2
 
 # delete folders
 [ -d $REPO_NAME ] && rm -rf $REPO_NAME
+[ -d $NEW_REPO_PATH ] && rm -rf $NEW_REPO_PATH
 
 # deploy new DAO that will be upgraded
 deploy_DAO_and_repo
@@ -62,16 +64,21 @@ upgrade_DAO 2
 
 echo "***** new repo07 deploy *****"
 gosh-cli call --abi $WALLET_ABI --sign $WALLET_KEYS $WALLET_ADDR deployRepository \
-    "{\"nameRepo\":\"$REPO_NAME\", \"previous\":{\"addr\":\"$REPO_ADDR\", \"version\":\"$TEST_VERSION1\"}}" || exit 1
+    "{\"nameRepo\":\"$REPO_NAME\", \"previous\":{\"addr\":\"$REPO_ADDR\", \"version\":\"$CUR_VERSION\"}}" || exit 1
 REPO_ADDR=$(gosh-cli -j run $SYSTEM_CONTRACT_ADDR_2 getAddrRepository "{\"name\":\"$REPO_NAME\",\"dao\":\"$DAO_NAME\"}" --abi $SYSTEM_CONTRACT_ABI | sed -n '/value0/ p' | cut -d'"' -f 4)
 
 echo "***** awaiting repo deploy *****"
 wait_account_active $REPO_ADDR
 sleep 3
 
-cd $REPO_NAME
-echo "**** Fetch repo *****"
-git fetch
+export NEW_LINK="gosh::$NETWORK://$SYSTEM_CONTRACT_ADDR_2/$DAO_NAME/$REPO_NAME"
+echo "NEW_LINK=$NEW_LINK"
+
+echo "***** cloning repo with new link *****"
+git clone $NEW_LINK $NEW_REPO_PATH
+
+echo "***** push to new version *****"
+cd $NEW_REPO_PATH
 
 cur_ver=$(cat 1.txt)
 if [ $cur_ver != "cur_ver" ]; then
@@ -85,8 +92,7 @@ git add 1.txt
 git commit -m test2
 git push
 
-git checkout -b parent_branch $PARENT_COMMIT_ID || true
-if [ $? = 0 ]; then
+if git checkout -b parent_branch $PARENT_COMMIT_ID; then
   echo "Branch created from wrong commit id"
   exit 1
 fi
