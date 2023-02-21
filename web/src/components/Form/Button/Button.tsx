ebc6aed47f3fa3c9ede37574462a1da0c6b710d2
rymka1989 <rymka1989@gmail.com>
2023-02-21 13:14:44 +0300

    [web] - try to fix custom buttons

--- web/src/components/Form/Button/Button.tsx
+++ web/src/components/Form/Button/Button.tsx
@@ -3,22 +3,33 @@ import Spinner from '../../Spinner'
 
 type TButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
     isLoading?: boolean
+    variant?: 'default' | 'custom' | 'outline-success'
 }
 
 const Button = (props: TButtonProps) => {
-    const { isLoading, className, children, disabled, ...rest } = props
+    const {
+        isLoading,
+        className,
+        children,
+        disabled,
+        variant = 'default',
+        ...rest
+    } = props
+
+    const styles = [
+        'text-sm text-center whitespace-nowrap',
+        'border border-transparent rounded-lg',
+        'py-2 px-4',
+        'disabled:pointer-events-none',
+    ]
+
+    if (variant === 'default') {
+        styles.push('bg-black text-white hover:text-white/75 disabled:text-gray-7c8db5')
+    }
 
     return (
         <button
-            className={classNames(
-                'bg-black text-white rounded-lg',
-                'text-sm text-center whitespace-nowrap',
-                'border border-transparent',
-                'py-2 px-4',
-                'hover:text-white/75',
-                'disabled:text-gray-7c8db5 disabled:bg-gray-fafafd disabled:pointer-events-none',
-                className,
-            )}
+            className={classNames(...styles, className)}
             {...rest}
             disabled={disabled || isLoading}
         >
