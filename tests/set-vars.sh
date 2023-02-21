6ac5863f64d51fdda9aa55ee2be937cd99edcc58
SilkovAlexander <slexander.s@tonlabs.io>
2023-02-21 23:09:16 +0300

    Fixed tonos -> gosh

--- tests/set-vars.sh
+++ tests/set-vars.sh
@@ -19,7 +19,7 @@ TEST_INDEX="${TEST_INDEX:-$(date +%s)}"
 echo "TEST INDEX $TEST_INDEX"
 echo "export NETWORK=$NETWORK" > env.env
 
-tonos-cli config --url $NETWORK
+gosh-cli config -e $NETWORK
 set -x
 # Should exist VersionConrtroler and SystemContract contracts
 
@@ -38,16 +38,16 @@ echo "export DAO_ABI=$DAO_ABI" >> env.env
 
 # could be changed to genphrase --dump
 
-SEED=`tonos-cli genphrase | grep -o '".*"' | tr -d '"'`
+SEED=`gosh-cli genphrase | grep -o '".*"' | tr -d '"'`
 echo $SEED > user.seed
 DAO_KEYS=test.keys.json
-tonos-cli getkeypair -o $DAO_KEYS -p "$SEED"
+gosh-cli getkeypair -o $DAO_KEYS -p "$SEED"
 DAO_PUBKEY=$(cat $DAO_KEYS | sed -n '/public/ s/.*\([[:xdigit:]]\{64\}\).*/0x\1/p')
 
 # *create Profile
 USER_PROFILE_NAME=user$TEST_INDEX
-tonos-cli call --abi $SYSTEM_CONTRACT_ABI $SYSTEM_CONTRACT_ADDR deployProfile "{\"pubkey\":\"$DAO_PUBKEY\",\"name\":\"$USER_PROFILE_NAME\"}"
-USER_PROFILE_ADDR=$(tonos-cli -j run $SYSTEM_CONTRACT_ADDR getProfileAddr "{\"name\":\"$USER_PROFILE_NAME\"}" --abi $SYSTEM_CONTRACT_ABI | sed -n '/value0/ p' | cut -d'"' -f 4)
+gosh-cli call --abi $SYSTEM_CONTRACT_ABI $SYSTEM_CONTRACT_ADDR deployProfile "{\"pubkey\":\"$DAO_PUBKEY\",\"name\":\"$USER_PROFILE_NAME\"}"
+USER_PROFILE_ADDR=$(gosh-cli -j run $SYSTEM_CONTRACT_ADDR getProfileAddr "{\"name\":\"$USER_PROFILE_NAME\"}" --abi $SYSTEM_CONTRACT_ABI | sed -n '/value0/ p' | cut -d'"' -f 4)
 wait_account_active $USER_PROFILE_ADDR
 echo "export USER_PROFILE_ABI=$USER_PROFILE_ABI" >> env.env
 echo "export USER_PROFILE_ADDR=$USER_PROFILE_ADDR" >> env.env
@@ -58,9 +58,9 @@ wait_account_active $USER_PROFILE_ADDR
 # *deploy DAO
 export DAO_NAME=dao-$TEST_INDEX
 echo "export DAO_NAME=$DAO_NAME" >> env.env
-tonos-cli call --abi $USER_PROFILE_ABI $USER_PROFILE_ADDR --sign $DAO_KEYS deployDao \
+gosh-cli call --abi $USER_PROFILE_ABI $USER_PROFILE_ADDR --sign $DAO_KEYS deployDao \
   "{\"systemcontract\":\"$SYSTEM_CONTRACT_ADDR\", \"name\":\"$DAO_NAME\", \"pubmem\":[\"$USER_PROFILE_ADDR\"]}"
-DAO_ADDR=$(tonos-cli -j run $SYSTEM_CONTRACT_ADDR getAddrDao "{\"name\":\"$DAO_NAME\"}" --abi $SYSTEM_CONTRACT_ABI | sed -n '/value0/ p' | cut -d'"' -f 4)
+DAO_ADDR=$(gosh-cli -j run $SYSTEM_CONTRACT_ADDR getAddrDao "{\"name\":\"$DAO_NAME\"}" --abi $SYSTEM_CONTRACT_ABI | sed -n '/value0/ p' | cut -d'"' -f 4)
 wait_account_active $DAO_ADDR
 
 echo "***** awaiting dao deploy *****"
@@ -72,18 +72,18 @@ echo "export WALLET_KEYS=$WALLET_KEYS" >> env.env
 WALLET_PUBKEY=$(cat $WALLET_KEYS | sed -n '/public/ s/.*\([[:xdigit:]]\{64\}\).*/0x\1/p')
 echo "export WALLET_PUBKEY=$WALLET_PUBKEY" >> env.env
 
-WALLET_ADDR=$(tonos-cli -j run $DAO_ADDR getAddrWallet "{\"pubaddr\":\"$USER_PROFILE_ADDR\",\"index\":0}" --abi $DAO_ABI | sed -n '/value0/ p' | cut -d'"' -f 4)
+WALLET_ADDR=$(gosh-cli -j run $DAO_ADDR getAddrWallet "{\"pubaddr\":\"$USER_PROFILE_ADDR\",\"index\":0}" --abi $DAO_ABI | sed -n '/value0/ p' | cut -d'"' -f 4)
 export WALLET_ADDR
 echo "export WALLET_ADDR=$WALLET_ADDR" >> env.env
 export WALLET_ABI=$GOSH_PATH/goshwallet.abi.json
 echo "export WALLET_ABI=$WALLET_ABI" >> env.env
 
-tonos-cli call --abi $USER_PROFILE_ABI $USER_PROFILE_ADDR --sign $WALLET_KEYS turnOn \
+gosh-cli call --abi $USER_PROFILE_ABI $USER_PROFILE_ADDR --sign $WALLET_KEYS turnOn \
   "{\"pubkey\":\"$WALLET_PUBKEY\",\"wallet\":\"$WALLET_ADDR\"}"
 
 sleep 10
 
-GRANTED_PUBKEY=$(tonos-cli -j run --abi $WALLET_ABI $WALLET_ADDR getAccess {} | jq -r .value0)
+GRANTED_PUBKEY=$(gosh-cli -j run --abi $WALLET_ABI $WALLET_ADDR getAccess {} | jq -r .value0)
 
 USER_CONFIG=$(pwd)/config.json
 echo "export GOSH_CONFIG_PATH=$USER_CONFIG" >> env.env
