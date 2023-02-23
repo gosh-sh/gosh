9c4de98e8d26d022bf9f44e3a526f7a6da11d5e0
SilkovAlexander <slexander.s@tonlabs.io>
2023-02-24 00:26:09 +0300

    Uncommented tests

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -13,23 +13,23 @@ set -o pipefail
 #  ./node_se_scripts/deploy.sh
 #fi
 
-#. set-vars.sh
-#. build_remote.sh
-#./01-clone_empty_repo.test.sh
-#./02-create_branch.test.sh
-#./03-push_multiple_updates_in_a_single_commit.test.sh
-#./04-push_parallel_diffs_in_multiple_commite.test.sh
-#./05-bin_test.test.sh
-#./06-sequential_merges.test.sh
-#./07-push_to_empty_main.test.sh
-#./08-repo_with_canceled_diffs.test.sh
-#./09-ipfs_onchain_transition.test.sh  !!!!!!!!!!!!
-#./10-ensure_blobs_onchain.test.sh
-#./11-git_submodules.test.sh
-#./12-clone_tree_with_rename.sh
-#./13_push_protected_branch.test.sh
-#./14_pull_request_details.test.sh
-#./15-diamond_merge_several_commits.test.sh
+. set-vars.sh
+. build_remote.sh
+./01-clone_empty_repo.test.sh
+./02-create_branch.test.sh
+./03-push_multiple_updates_in_a_single_commit.test.sh
+./04-push_parallel_diffs_in_multiple_commite.test.sh
+./05-bin_test.test.sh
+./06-sequential_merges.test.sh
+./07-push_to_empty_main.test.sh
+./08-repo_with_canceled_diffs.test.sh
+./09-ipfs_onchain_transition.test.sh
+./10-ensure_blobs_onchain.test.sh
+./11-git_submodules.test.sh
+./12-clone_tree_with_rename.sh
+./13_push_protected_branch.test.sh
+./14_pull_request_details.test.sh
+./15-diamond_merge_several_commits.test.sh
 ./16-push_after_diamond_merge.test.sh
 ./17-create_file_in_branch.test.sh
 
