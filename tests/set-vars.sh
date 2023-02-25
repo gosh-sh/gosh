0f7c32c06fdae451c70b303b9cdb405540107f2f
sostrovskyi <sostrovskyi>
2023-02-25 14:34:31 +0000

    fix ci dispatcher

--- tests/set-vars.sh
+++ tests/set-vars.sh
@@ -36,14 +36,10 @@ echo "export DAO_ABI=$DAO_ABI" >> env.env
 
 # generate user keys
 
-# could be changed to genphrase --dump
-
-#SEED=`tonos-cli genphrase | jq .phrase`
-#echo $SEED > user.seed
-#tonos-cli getkeypair -o $DAO_KEYS -p "$SEED"
-DAO_KEYS=test.keys.json
-SEED=`tonos-cli genphrase --dump $DAO_KEYS | jq .phrase`
+SEED=`tonos-cli genphrase | jq .phrase`
 echo $SEED > user.seed
+DAO_KEYS=test.keys.json
+tonos-cli getkeypair -o $DAO_KEYS -p "$SEED"
 DAO_PUBKEY=$(cat $DAO_KEYS | sed -n '/public/ s/.*\([[:xdigit:]]\{64\}\).*/0x\1/p')
 
 # *create Profile
