ff13fafcda55c2e9eecd69da186a52212e68e98e
Sergei Blinov <blinovsv@gmail.com>
2023-03-02 10:16:24 +0100

    update install script

--- install.sh
+++ install.sh
@@ -4,26 +4,26 @@ set -e
 
 REPO_OWNER=gosh-sh
 REPO=gosh
-RELEASE=rc-3.0.10
+RELEASE=rc-3.0.12
 # TODO: get it from one source with binary
-SUPPORTED_CONTRACTS_VERSIONS=("2_0_0")
+SUPPORTED_CONTRACTS_VERSIONS=("2_0_0" "1_0_0")
 
 # Check OS and architecture
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    if [[ `uname -m` == "x86_64" ]]; then
+    if [[ $(uname -m) == "x86_64" ]]; then
         TAR="git-remote-gosh-linux-amd64.tar"
     else
         TAR="git-remote-gosh-linux-arm64.tar"
     fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    if [[ `uname -m` == "x86_64" ]]; then
+    if [[ $(uname -m) == "x86_64" ]]; then
         TAR="git-remote-gosh-darwin-amd64.tar"
     else
         TAR="git-remote-gosh-darwin-arm64.tar"
     fi
 else
-  echo "Only \"MacOS\" and \"Linux\" are supported - not \"$OSTYPE\""
-  exit 1;
+    echo "Only \"MacOS\" and \"Linux\" are supported - not \"$OSTYPE\""
+    exit 1
 fi
 
 TAG=$(echo $RELEASE | tr -d 'rc-')
@@ -33,8 +33,8 @@ GH_TAGS="$GH_REPO/releases/tags/$TAG"
 
 # dir
 
-mkdir -p $HOME/.gosh
-echo git-remote-gosh_v${SUPPORTED_CONTRACTS_VERSIONS[0]} > $HOME/.gosh/dispatcher.ini
+mkdir -p "$HOME"/.gosh
+echo git-remote-gosh_v"${SUPPORTED_CONTRACTS_VERSIONS[0]}" >"$HOME"/.gosh/dispatcher.ini
 
 # Download release
 echo ""
@@ -42,11 +42,14 @@ echo "Downloading $EXECUTABLE release \"$TAG\""
 echo ""
 
 # Read asset tags.
-response=$(curl -s $GH_TAGS)
+response=$(curl -s "$GH_TAGS")
 
 # Get ID of the asset based on given name.
 eval $(echo "$response" | grep -C3 "name.:.\+$TAR" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
-[ "$id" ] || { echo "Error: Failed to get asset id, response: $response" | awk 'length($0)<100' >&2; exit 1; }
+[ "$id" ] || {
+    echo "Error: Failed to get asset id, response: $response" | awk 'length($0)<100' >&2
+    exit 1
+}
 
 wget --content-disposition --no-cookie -q --header "Accept: application/octet-stream" "$GH_REPO/releases/assets/$id" --show-progress
 
@@ -54,15 +57,15 @@ wget --content-disposition --no-cookie -q --header "Accept: application/octet-st
 tar -xf $TAR
 rm -f $TAR
 
-# make executable 
+# make executable
 chmod +x git-remote-gosh
-chmod +x git-remote-gosh_v${SUPPORTED_CONTRACTS_VERSIONS[0]}
+chmod +x git-remote-gosh_v"${SUPPORTED_CONTRACTS_VERSIONS[0]}"
 
-mv git-remote-gosh $HOME/.gosh/
-mv git-remote-gosh_v${SUPPORTED_CONTRACTS_VERSIONS[0]} $HOME/.gosh/
+mv git-remote-gosh "$HOME"/.gosh/
+mv git-remote-gosh_v"${SUPPORTED_CONTRACTS_VERSIONS[0]}" $HOME/.gosh/
 
-echo "export PATH=\$PATH:\$HOME/.gosh" >> $HOME/.bash_profile
-echo "export PATH=\$PATH:\$HOME/.gosh" >> $HOME/.bashrc
+echo "export PATH=\$PATH:\$HOME/.gosh" >>"$HOME"/.bash_profile
+echo "export PATH=\$PATH:\$HOME/.gosh" >>"$HOME"/.bashrc
 
 echo ======================================================
 echo 'Run the following command to finish your installation:'
