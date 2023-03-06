9f70f40af1a2aaab61b0386e64438e9e7f00f3d4
SilkovAlexander <slexander.s@tonlabs.io>
2023-03-06 16:20:02 +0300

    Removed extra script and added some tests to ignore

--- tests/test_upgrade.sh
+++ tests/test_upgrade.sh
@@ -16,22 +16,21 @@ set -o pipefail
 # $1 = VERSION (v1_x, v2_x)  ignore
 # $2 = NETWORK
 
+# put under flag deploy of 9999
 . set-vars.sh v1_x $2
-#. build_remote.sh
-
-./upgrade_tests/set_up_v2.sh
+./upgrade_tests/set_up.sh
 ./upgrade_tests/01-clone_rewritten_repo.test.sh
 ./upgrade_tests/02_1-clone_upgraded_repo.test.sh
 ./upgrade_tests/02_2-push_after_upgrade.test.sh
 ./upgrade_tests/02_3-push_after_upgrade_with_several_commits.test.sh
 ./upgrade_tests/03-branch_from_parent.test.sh
-./upgrade_tests/04-branch_from_grandparent.test.sh
+./upgrade_tests/04-branch_from_grandparent.test.sh ignore
 ./upgrade_tests/05-merge_branch_from_parent.test.sh
 ./upgrade_tests/05_1-merge_branch_from_parent_with_several_commits.test.sh
 ./upgrade_tests/05_2-push_after_merge.test.sh
 ./upgrade_tests/05_3-create_file_in_branch.test.sh
 ./upgrade_tests/06-merge_branch_from_grandparent.test.sh ignore
-./upgrade_tests/07-branch_from_unrelated_commit.test.sh
+./upgrade_tests/07-branch_from_unrelated_commit.test.sh ignore
 ./upgrade_tests/08-tagging_after_upgrade.test.sh ignore
 ./upgrade_tests/09-delete_tag_after_upgrade.test.sh ignore
 ./upgrade_tests/10-tagless_after_upgrade.test.sh ignore
--- tests/upgrade_tests/set_up.sh
+++ tests/upgrade_tests/set_up.sh
@@ -4,71 +4,72 @@ set -o pipefail
 . ./util.sh
 
 set -x
-
-GOSH_PATH=../contracts/gosh
-TESTS_PATH=../../tests
-CUR_VERSION=$(grep -r 'string constant version' $GOSH_PATH/systemcontract.sol | sed 's/^.*[^0-9]\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/')
-TEST_VERSION1=9998.0.0
+TESTS_PATH=$PWD
+GOSH_PATH=../v2_x/contracts/gosh
+OLD_GOSH_PATH=../v1_x/contracts/gosh
+CUR_VERSION=$(grep -r 'string constant version' $OLD_GOSH_PATH/systemcontract.sol | sed 's/^.*[^0-9]\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/')
+TEST_VERSION1=$(grep -r 'string constant version' $GOSH_PATH/systemcontract.sol | sed 's/^.*[^0-9]\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/')
 TEST_VERSION2=9999.0.0
 
 echo "export CUR_VERSION=$CUR_VERSION" >> env.env
 echo "export TEST_VERSION1=$TEST_VERSION1" >> env.env
-echo "export TEST_VERSION2=$TEST_VERSION2" >> env.env
+echo "export TEST_VERSION2=9999.0.0" >> env.env
 
 export NETWORK="${NETWORK:-http://192.168.31.227}"
+echo "NETWORK=$NETWORK"
+
 SE_GIVER_ADDRESS="0:ece57bcc6c530283becbbd8a3b24d3c5987cdddc3c8b7b33be6e4a6312490415"
 SE_GIVER_ABI="$TESTS_PATH/node_se_scripts/local_giver.abi.json"
 SE_GIVER_KEYS="$TESTS_PATH/node_se_scripts/local_giver.keys.json"
 GIVER_VALUE=20000000000000000
-CLI_CONF_PATH=$PWD/gosh-cli.conf.json
 
-if [ "$CUR_VERSION" = "$TEST_VERSION1" ] || [ "$CUR_VERSION" = "$TEST_VERSION2" ]; then
-  echo "Current contract version is equal to one of the reserved for tests"
-  exit 1
-fi
+export VERSIONCONTROLLER_ADDR=`cat $OLD_GOSH_PATH/VersionController.addr`
+VERSIONCONTROLLER_SEED=`cat $OLD_GOSH_PATH/gosh.seed | grep -o '".*"' | tr -d '"'`
+export VERSIONCONTROLLER_SEED="$VERSIONCONTROLLER_SEED"
 
 ### TODO: script is now configured to work with Node SE. Mb add variants for other networks
 echo "Start upgrade tests set up"
 
 echo "Deploy giver for upgrade"
 cd $GOSH_PATH/../multisig
-gosh-cli -c "$CLI_CONF_PATH" config -e localhost
+
 make generate-docker
 export GIVER_ADDR=`cat Giver.addr`
 echo "GIVER_ADDR = $GIVER_ADDR"
-gosh-cli -c "$CLI_CONF_PATH" callx --abi $SE_GIVER_ABI --addr $SE_GIVER_ADDRESS --keys $SE_GIVER_KEYS -m sendTransaction --value $GIVER_VALUE --bounce false --dest $GIVER_ADDR
+tonos-cli -u "$NETWORK" callx --abi $SE_GIVER_ABI --addr $SE_GIVER_ADDRESS --keys $SE_GIVER_KEYS -m sendTransaction --value $GIVER_VALUE --bounce false --dest $GIVER_ADDR
 
 make deploy-docker
 
 echo "Upgrade to test version $TEST_VERSION1"
 cd ../gosh
 
-sed -i "s/version = \"$CUR_VERSION/version = \"$TEST_VERSION1/" *.sol
 
-CHECK_VERSION=$(grep -r 'string constant version' systemcontract.sol | sed 's/^.*[^0-9]\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/')
-if [ $CHECK_VERSION != $TEST_VERSION1 ]; then
-  echo "Failed to change contract version"
-  exit 1
-fi
-
-make build
-export VERSIONCONTROLLER_ADDR=`cat VersionController.addr`
-VERSIONCONTROLLER_SEED=`cat gosh.seed | grep -o '".*"' | tr -d '"'`
-export VERSIONCONTROLLER_SEED="$VERSIONCONTROLLER_SEED"
 echo > SystemContract-${TEST_VERSION1}.addr
 make upgrade-docker
 
 VERSIONCONTROLLER_ABI=versioncontroller.abi.json
 
-SYSTEM_CONTRACT_ADDR_1=$(gosh-cli -c "$CLI_CONF_PATH" -j run $VERSIONCONTROLLER_ADDR getSystemContractAddr "{\"version\":\"$TEST_VERSION1\"}" --abi $VERSIONCONTROLLER_ABI | sed -n '/value0/ p' | cut -d'"' -f 4)
+SYSTEM_CONTRACT_ADDR_1=$(tonos-cli -u "$NETWORK" -j run $VERSIONCONTROLLER_ADDR getSystemContractAddr "{\"version\":\"$TEST_VERSION1\"}" --abi $VERSIONCONTROLLER_ABI | sed -n '/value0/ p' | cut -d'"' -f 4)
 echo "SYSTEM_CONTRACT_ADDR_1=$SYSTEM_CONTRACT_ADDR_1"
 
+cd $TESTS_PATH
+echo "export SYSTEM_CONTRACT_ADDR_1=$SYSTEM_CONTRACT_ADDR_1" >> env.env
+
+tonos-cli -u "$NETWORK" runx --addr $SYSTEM_CONTRACT_ADDR_1 --abi $SYSTEM_CONTRACT_ABI -m getVersion
+
+PROP_ID=$(tvm_linker test node_se_scripts/prop_id_gen --gas-limit 1000000 \
+  --abi-json node_se_scripts/prop_id_gen.abi.json --abi-method get_upgrade_prop_id --abi-params \
+  "{\"newversion\":\"$TEST_VERSION1\",\"description\":\"\"}"  --decode-c6 | grep value0 \
+  | sed -n '/value0/ p' | cut -d'"' -f 4)
+echo "export PROP_ID1=\"$PROP_ID\"" >> env.env
+
+
 echo "Deploy giver for upgrade"
-cd ../multisig
+cd $GOSH_PATH/../multisig
 make generate-docker
 export GIVER_ADDR=`cat Giver.addr`
 echo "GIVER_ADDR = $GIVER_ADDR"
-gosh-cli -c "$CLI_CONF_PATH" callx --abi $SE_GIVER_ABI --addr $SE_GIVER_ADDRESS --keys $SE_GIVER_KEYS -m sendTransaction --value $GIVER_VALUE --bounce false --dest $GIVER_ADDR
+tonos-cli -u "$NETWORK" callx --abi $SE_GIVER_ABI --addr $SE_GIVER_ADDRESS --keys $SE_GIVER_KEYS -m sendTransaction --value $GIVER_VALUE --bounce false --dest $GIVER_ADDR
 
 make deploy-docker
 
@@ -84,24 +85,22 @@ if [ $CHECK_VERSION != $TEST_VERSION2 ]; then
 fi
 
 make build
+
 echo > SystemContract-${TEST_VERSION2}.addr
 make upgrade-docker
 
-SYSTEM_CONTRACT_ADDR_2=$(gosh-cli -c "$CLI_CONF_PATH" -j run $VERSIONCONTROLLER_ADDR getSystemContractAddr "{\"version\":\"$TEST_VERSION2\"}" --abi $VERSIONCONTROLLER_ABI | sed -n '/value0/ p' | cut -d'"' -f 4)
-echo "SYSTEM_CONTRACT_ADDR_2=$SYSTEM_CONTRACT_ADDR_2"
-
-sed -i "s/version = \"$TEST_VERSION2/version = \"$CUR_VERSION/" *.sol
+sed -i "s/version = \"$TEST_VERSION2/version = \"$TEST_VERSION1/" *.sol
 make build
 
+SYSTEM_CONTRACT_ADDR_2=$(tonos-cli -u "$NETWORK" -j run $VERSIONCONTROLLER_ADDR getSystemContractAddr "{\"version\":\"$TEST_VERSION2\"}" --abi $VERSIONCONTROLLER_ABI | sed -n '/value0/ p' | cut -d'"' -f 4)
+echo "SYSTEM_CONTRACT_ADDR_2=$SYSTEM_CONTRACT_ADDR_2"
+
 cd $TESTS_PATH
-echo "export SYSTEM_CONTRACT_ADDR_1=$SYSTEM_CONTRACT_ADDR_1" >> env.env
 echo "export SYSTEM_CONTRACT_ADDR_2=$SYSTEM_CONTRACT_ADDR_2" >> env.env
 
-gosh-cli -c "$CLI_CONF_PATH" runx --addr $SYSTEM_CONTRACT_ADDR_1 --abi $SYSTEM_CONTRACT_ABI -m getVersion
-gosh-cli -c "$CLI_CONF_PATH" runx --addr $SYSTEM_CONTRACT_ADDR_2 --abi $SYSTEM_CONTRACT_ABI -m getVersion
-
-# setup prop_id for versions 9998 9999
-echo "export PROP_ID1=\"3950845175897358018094131615753860290307075417829333502875089611607422753737\"" >> env.env
-echo "export PROP_ID2=\"88364137685699556084928035923009541681801570983404644442401696785754621762666\"" >> env.env
+echo "export SYSTEM_CONTRACT_ABI_1=../v2_x/contracts/gosh/systemcontract.abi.json" >> env.env
+echo "export REPO_ABI_1=../v2_x/contracts/gosh/repository.abi.json" >> env.env
+echo "export DAO_ABI_1=../v2_x/contracts/gosh/goshdao.abi.json" >> env.env
+echo "export WALLET_ABI_1=../v2_x/contracts/gosh/goshwallet.abi.json" >> env.env
 
 echo "SET UP SUCCEEDED"
