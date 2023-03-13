b65eaf6fb167be95801dda130419ab2507a8da4e
SilkovAlexander <slexander.s@tonlabs.io>
2023-03-13 17:36:10 +0300

    Fixed after merge

--- tests/upgrade_tests/10-tagless_after_upgrade.test.sh
+++ tests/upgrade_tests/10-tagless_after_upgrade.test.sh
@@ -47,8 +47,9 @@ upgrade_DAO
 
 echo "***** new $REPO_NAME deploy *****"
 tonos-cli call --abi $WALLET_ABI_1 --sign $WALLET_KEYS $WALLET_ADDR AloneDeployRepository \
-    "{\"nameRepo\":\"$REPO_NAME\",\"descr\":\"\",\"previous\":{\"addr\":\"$REPO_ADDR\", \"version\":\"$TEST_VERSION1\"}}" || exit 1
+    "{\"nameRepo\":\"$REPO_NAME\",\"descr\":\"\",\"previous\":{\"addr\":\"$REPO_ADDR\", \"version\":\"$CUR_VERSION\"}}" || exit 1
 REPO_ADDR=$(tonos-cli -j run $SYSTEM_CONTRACT_ADDR_1 getAddrRepository "{\"name\":\"$REPO_NAME\",\"dao\":\"$DAO_NAME\"}" --abi $SYSTEM_CONTRACT_ABI | sed -n '/value0/ p' | cut -d'"' -f 4)
+echo "REPO_ADDR=$REPO_ADDR"
 
 echo "***** awaiting repo deploy *****"
 wait_account_active $REPO_ADDR
