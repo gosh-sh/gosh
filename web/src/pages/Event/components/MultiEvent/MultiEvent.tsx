809d48827fc2098cef40b362719b6e8abc6c0b77
rymka1989 <rymka1989@gmail.com>
2023-03-18 00:57:00 +0300

    [web] - fix single dao member add

--- web/src/pages/Event/components/MultiEvent/MultiEvent.tsx
+++ web/src/pages/Event/components/MultiEvent/MultiEvent.tsx
@@ -30,74 +30,85 @@ const MultiEvent = (props: TMultiEventProps) => {
 
     return (
         <div className="flex flex-col divide-y divide-gray-e6edff">
-            {event.data.map((data: any, index: number) => (
-                <div className="py-3" key={index}>
-                    <h3>{data.type.name}</h3>
-                    {data.type.kind === ESmvEventType.REPO_CREATE && (
-                        <RepoCreateEvent key={index} data={data} />
-                    )}
-                    {data.type.kind === ESmvEventType.BRANCH_LOCK && (
-                        <RepoBranchEvent key={index} type={data.type} data={data} />
-                    )}
-                    {data.type.kind === ESmvEventType.BRANCH_UNLOCK && (
-                        <RepoBranchEvent key={index} type={data.type} data={data} />
-                    )}
-                    {data.type.kind === ESmvEventType.REPO_TAG_ADD && (
-                        <RepoTagAddEvent key={index} data={data} />
-                    )}
-                    {data.type.kind === ESmvEventType.REPO_TAG_REMOVE && (
-                        <RepoTagRemoveEvent key={index} data={data} />
-                    )}
-                    {data.type.kind === ESmvEventType.REPO_UPDATE_DESCRIPTION && (
-                        <RepoDescriptionEvent key={index} data={data} />
-                    )}
-                    {data.type.kind === ESmvEventType.DAO_MEMBER_ADD && (
-                        <MemberAddEvent key={index} version={version} data={data} />
-                    )}
-                    {data.type.kind === ESmvEventType.DAO_MEMBER_DELETE && (
-                        <MemberRemoveEvent key={index} data={data} />
-                    )}
-                    {data.type.kind === ESmvEventType.DAO_TOKEN_VOTING_ADD && (
-                        <DaoTokenVotingAddEvent key={index} data={data} />
-                    )}
-                    {data.type.kind === ESmvEventType.DAO_TOKEN_REGULAR_ADD && (
-                        <DaoTokenRegularAddEvent key={index} data={data} />
-                    )}
-                    {data.type.kind === ESmvEventType.DAO_TAG_ADD && (
-                        <DaoTagAddEvent key={index} data={data} />
-                    )}
-                    {data.type.kind === ESmvEventType.DAO_TAG_REMOVE && (
-                        <DaoTagRemoveEvent key={index} data={data} />
-                    )}
-                    {data.type.kind === ESmvEventType.DAO_EVENT_ALLOW_DISCUSSION && (
-                        <DaoEventAllowDiscussionEvent key={index} data={data} />
-                    )}
-                    {data.type.kind === ESmvEventType.DAO_EVENT_HIDE_PROGRESS && (
-                        <DaoEventShowProgressEvent key={index} data={data} />
-                    )}
-                    {data.type.kind === ESmvEventType.DAO_ASK_MEMBERSHIP_ALLOWANCE && (
-                        <DaoAskMembershipAllowanceEvent key={index} data={data} />
-                    )}
-                    {data.type.kind === ESmvEventType.DAO_TOKEN_MINT_DISABLE && (
-                        <DaoMintDisableEvent key={index} data={data} />
-                    )}
-                    {data.type.kind === ESmvEventType.DAO_TOKEN_MINT && (
-                        <DaoMintReserveEvent key={index} data={data} />
-                    )}
-                    {data.type.kind === ESmvEventType.DAO_UPGRADE && (
-                        <DaoUpgradeEvent key={index} data={data} />
-                    )}
-                    {data.type.kind === ESmvEventType.DAO_ALLOWANCE_CHANGE && (
-                        <MemberAllowanceEvent key={index} data={data} />
-                    )}
-                    {data.type.kind === ESmvEventType.TASK_CREATE && (
-                        <TaskCreateEvent key={index} data={data} />
-                    )}
-                    {data.type.kind === ESmvEventType.TASK_DELETE && (
-                        <TaskDeleteEvent key={index} data={data} />
-                    )}
-                </div>
-            ))}
+            {event.data
+                .filter((data: any) => {
+                    if (
+                        data.type.kind === ESmvEventType.DAO_TOKEN_MINT &&
+                        parseInt(data.grant) === 0
+                    ) {
+                        return false
+                    }
+                    return true
+                })
+                .map((data: any, index: number) => (
+                    <div className="py-3" key={index}>
+                        <h3>{data.type.name}</h3>
+                        {data.type.kind === ESmvEventType.REPO_CREATE && (
+                            <RepoCreateEvent key={index} data={data} />
+                        )}
+                        {data.type.kind === ESmvEventType.BRANCH_LOCK && (
+                            <RepoBranchEvent key={index} type={data.type} data={data} />
+                        )}
+                        {data.type.kind === ESmvEventType.BRANCH_UNLOCK && (
+                            <RepoBranchEvent key={index} type={data.type} data={data} />
+                        )}
+                        {data.type.kind === ESmvEventType.REPO_TAG_ADD && (
+                            <RepoTagAddEvent key={index} data={data} />
+                        )}
+                        {data.type.kind === ESmvEventType.REPO_TAG_REMOVE && (
+                            <RepoTagRemoveEvent key={index} data={data} />
+                        )}
+                        {data.type.kind === ESmvEventType.REPO_UPDATE_DESCRIPTION && (
+                            <RepoDescriptionEvent key={index} data={data} />
+                        )}
+                        {data.type.kind === ESmvEventType.DAO_MEMBER_ADD && (
+                            <MemberAddEvent key={index} version={version} data={data} />
+                        )}
+                        {data.type.kind === ESmvEventType.DAO_MEMBER_DELETE && (
+                            <MemberRemoveEvent key={index} data={data} />
+                        )}
+                        {data.type.kind === ESmvEventType.DAO_TOKEN_VOTING_ADD && (
+                            <DaoTokenVotingAddEvent key={index} data={data} />
+                        )}
+                        {data.type.kind === ESmvEventType.DAO_TOKEN_REGULAR_ADD && (
+                            <DaoTokenRegularAddEvent key={index} data={data} />
+                        )}
+                        {data.type.kind === ESmvEventType.DAO_TAG_ADD && (
+                            <DaoTagAddEvent key={index} data={data} />
+                        )}
+                        {data.type.kind === ESmvEventType.DAO_TAG_REMOVE && (
+                            <DaoTagRemoveEvent key={index} data={data} />
+                        )}
+                        {data.type.kind === ESmvEventType.DAO_EVENT_ALLOW_DISCUSSION && (
+                            <DaoEventAllowDiscussionEvent key={index} data={data} />
+                        )}
+                        {data.type.kind === ESmvEventType.DAO_EVENT_HIDE_PROGRESS && (
+                            <DaoEventShowProgressEvent key={index} data={data} />
+                        )}
+                        {data.type.kind ===
+                            ESmvEventType.DAO_ASK_MEMBERSHIP_ALLOWANCE && (
+                            <DaoAskMembershipAllowanceEvent key={index} data={data} />
+                        )}
+                        {data.type.kind === ESmvEventType.DAO_TOKEN_MINT_DISABLE && (
+                            <DaoMintDisableEvent key={index} data={data} />
+                        )}
+                        {data.type.kind === ESmvEventType.DAO_TOKEN_MINT && (
+                            <DaoMintReserveEvent key={index} data={data} />
+                        )}
+                        {data.type.kind === ESmvEventType.DAO_UPGRADE && (
+                            <DaoUpgradeEvent key={index} data={data} />
+                        )}
+                        {data.type.kind === ESmvEventType.DAO_ALLOWANCE_CHANGE && (
+                            <MemberAllowanceEvent key={index} data={data} />
+                        )}
+                        {data.type.kind === ESmvEventType.TASK_CREATE && (
+                            <TaskCreateEvent key={index} data={data} />
+                        )}
+                        {data.type.kind === ESmvEventType.TASK_DELETE && (
+                            <TaskDeleteEvent key={index} data={data} />
+                        )}
+                    </div>
+                ))}
         </div>
     )
 }
