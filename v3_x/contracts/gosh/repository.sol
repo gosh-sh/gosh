4d84b439ffd08b8178e3333ce6431eef7315e13b
Sehor <gorelushev.sergey@yandex.ru>
2023-03-20 15:38:59 +0300

    fix

--- v3_x/contracts/gosh/repository.sol
+++ v3_x/contracts/gosh/repository.sol
@@ -109,6 +109,16 @@ contract Repository is Modifiers{
             _protectedBranch = protectedBranch;
             _head = head;
             this.smvdeployrepotagin{value: 0.1 ton}(hashtag.values());
+            return;
+        }
+        if (ver == "3.0.0"){
+            (bool ans, mapping(uint256 => Item) Branches, mapping(uint256 => bool) protectedBranch, string head, mapping(uint256 => string) hashtag) = abi.decode(a, (bool , mapping(uint256 => Item), mapping(uint256 => bool), string, mapping(uint256 => string)));
+            if (ans == false) { selfdestruct(giver); }
+            _Branches = Branches;
+            _protectedBranch = protectedBranch;
+            _head = head;
+            this.smvdeployrepotagin{value: 0.1 ton}(hashtag.values());
+            return;
         }
     }
     
