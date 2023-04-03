bb346fa06070be294eadfdee7725ac1630c1d60d
rymka1989 <rymka1989@gmail.com>
2023-04-04 00:17:15 +0300

    [web] - vite for build docker extension

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: './',
     envPrefix: 'REACT_',
     plugins: [react()],
     assetsInclude: ['**/*.md'],
@@ -12,6 +11,12 @@ export default defineConfig({
     },
     build: {
         outDir: 'build',
+    },
+    define: {
+        // For docker extension.
+        // Set global in `optimizeDeps.esbuildOptions.define`
+        // is not working in docker extension build
+        global: 'globalThis',
     },
     optimizeDeps: {
         esbuildOptions: {
