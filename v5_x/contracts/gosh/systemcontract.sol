15252edfce36e63c462ea8a295e03de871d462f4
sehor05 <gorelushev.sergey@yandex.ru>
2023-04-14 16:30:30 +0300

    update6

--- v5_x/contracts/gosh/systemcontract.sol
+++ v5_x/contracts/gosh/systemcontract.sol
@@ -24,7 +24,7 @@ import "./libraries/GoshLib.sol";
 
 /* System contract of Gosh version*/
 contract SystemContract is Modifiers {
-    string constant version = "4.0.0";
+    string constant version = "5.0.0";
     
     address _versionController;
     bool _flag = true;
