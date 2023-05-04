88b3204e8a6577ae0cf62293305c7cabfb20d33e
SilkovAlexander <alexander.s@gosh.sh>
2023-05-04 19:28:47 +0300

    Fix rc publish (#468)
    * Fix rc publish
    
    * Reverted ci
    
    ---------
    
    Co-authored-by: SilkovAlexander <slexander.s@tonlabs.io>

--- .ci/scripts/github-upload-assets.sh
+++ .ci/scripts/github-upload-assets.sh
@@ -6,11 +6,12 @@ set -o pipefail
 TOKEN=$1
 VERSION=$2
 ASSET=$3
+CONTENT_TYPE=$4
 
 UPLOAD_URL=$(curl -sH "Authorization: token ${TOKEN}" \
     "https://api.github.com/repos/gosh-sh/gosh/releases/tags/${VERSION}" | jq -r '.upload_url' | cut -d'{' -f1)
 
 curl -sX POST -H "Authorization: token ${TOKEN}" \
-    -H "Accept: application/vnd.github.v3+json" -H "Content-Type: $(file -b --mime-type "$ASSET")" \
+    -H "Accept: application/vnd.github.v3+json" -H "Content-Type: ${CONTENT_TYPE}" \
     -H "Content-Length: $(wc -c <"$ASSET" | xargs)" \
     -T "$ASSET" "${UPLOAD_URL}?name=$(basename "$ASSET")" | cat
