69f08cf3e750aa3357da7e9897dbc9dc075b8ab2
sostrovskyi <sostrovskyi>
2023-05-17 18:28:09 +0300

    simple deb build and release for amd64 and arm64

--- .ci/scripts/package-deb.sh
+++ .ci/scripts/package-deb.sh
@@ -3,30 +3,33 @@
 PKG_NAME="git-remote-gosh"
 BINARIES=("git-remote-gosh" "git-remote-gosh_v1_0_0" "git-remote-gosh_v2_0_0" "git-remote-gosh_v3_0_0" "git-remote-gosh_v4_0_0")
 DISPATCHER_FILE="dispatcher.ini"
+ARCHITECTURES=("amd64" "arm64")
 
-# Create necessary directories
-mkdir -p "${PKG_NAME}/DEBIAN"
-mkdir -p "${PKG_NAME}/usr/local/bin"
-mkdir -p "${PKG_NAME}/usr/local/share/git-remote-gosh"
+for arch in "${ARCHITECTURES[@]}"; do
+    # Create necessary directories
+    mkdir -p "${PKG_NAME}_${arch}/DEBIAN"
+    mkdir -p "${PKG_NAME}_${arch}/usr/local/bin"
+    mkdir -p "${PKG_NAME}_${arch}/usr/local/share/git-remote-gosh"
 
-# Copy binaries into package
-for binary in "${BINARIES[@]}"; do
-    cp "linux-amd64/${binary}" "${PKG_NAME}/usr/local/bin/"
-done
+    # Copy binaries into package
+    for binary in "${BINARIES[@]}"; do
+        cp "linux-${arch}/${binary}" "${PKG_NAME}_${arch}/usr/local/bin/"
+    done
 
-# Copy dispatcher.ini into package
-cp "linux-amd64/${DISPATCHER_FILE}" "${PKG_NAME}/usr/local/bin"
+    # Copy dispatcher.ini into package
+    cp "linux-${arch}/${DISPATCHER_FILE}" "${PKG_NAME}_${arch}/usr/local/bin"
 
-# Create control file
-cat << EOF > "${PKG_NAME}/DEBIAN/control"
+    # Create control file
+    cat << EOF > "${PKG_NAME}_${arch}/DEBIAN/control"
 Package: ${PKG_NAME}
 Version: 4.1.20
 Section: custom
 Priority: optional
-Architecture: amd64
+Architecture: ${arch}
 Maintainer: GOSH <hr@gosh.sh>
 Description: Git remote helper for GOSH repositories.
 EOF
 
-# Build Debian package (binary package)
-dpkg-deb --build "${PKG_NAME}"
+    # Build Debian package (binary package)
+    dpkg-deb --build "${PKG_NAME}_${arch}"
+done
