0f3dce625c49224cdd172bdf1d9b18f9955d3de5
roman.ec4a <rymka1989@gmail.com>
2023-07-09 14:55:38 +0300

    [web] - add member icon common component

---
+++ web/src/components/Dao/MemberIcon/MemberIcon.tsx
@@ -0,0 +1,27 @@
+import {
+    IconDefinition,
+    faBuildingColumns,
+    faEnvelope,
+    faUser,
+} from '@fortawesome/free-solid-svg-icons'
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon, FontAwesomeIconProps } from '@fortawesome/react-fontawesome'
+
+const icons: { [type: string]: IconDefinition } = {
+    user: faUser,
+    dao: faBuildingColumns,
+    email: faEnvelope,
+}
+
+type TMemberIconProps = Omit<FontAwesomeIconProps, 'icon'> & {
+    type: 'user' | 'dao' | 'email'
+    icon?: IconProp
+}
+
+const MemberIcon = (props: TMemberIconProps) => {
+    const { type, icon, ...rest } = props
+
+    return <FontAwesomeIcon icon={icon || icons[type]} {...rest} />
+}
+
+export default MemberIcon
