853e5483e9bbf226e670b05b9e3aa9370d5b6b1c
sehor05 <gorelushev.sergey@yandex.ru>
2023-08-14 18:15:05 +0300

    old fix

--- v6_x/v6.0.0/contracts/gosh/commit.sol
+++ v6_x/v6.0.0/contracts/gosh/commit.sol
@@ -409,9 +409,8 @@ contract Commit is Modifiers {
 
     function acceptAll(string branch, address branchCommit) public senderIs(address(this)) {
         if ((_commitcheck != false) && (_diffcheck != false)) {
-//            Repository(_rootRepo).setCommit{value: 0.3 ton, bounce: true , flag: 1}(branch, branchCommit, _nameCommit, _number, _numbercommits, _task); 
-//            _number = 0;
-            Tree(_tree).checkFull{value: 0.14 ton, flag:1}(_nameCommit, branch, _rootRepo, _nameCommit, TYPE_SET_COMMIT, branchCommit);
+            Repository(_rootRepo).setCommit{value: 0.3 ton, bounce: true , flag: 1}(branch, branchCommit, _nameCommit, _number, _numbercommits, _task); 
+//            Tree(_tree).checkFull{value: 0.14 ton, flag:1}(_nameCommit, branch, _rootRepo, _nameCommit, TYPE_SET_COMMIT, branchCommit);
         }
         else {
             _diffcheck = true;
