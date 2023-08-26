e2acd5c0c01502a7200f2865e2ec7069ec39669f
roman.ec4a <rymka1989@gmail.com>
2023-08-26 17:52:13 +0300

    [web] - fix dao member list markup

--- web/src/v1.0.0/pages/DaoMemberList/components/MemberList/ListItem.tsx
+++ web/src/v1.0.0/pages/DaoMemberList/components/MemberList/ListItem.tsx
@@ -11,12 +11,13 @@ import { MemberIcon } from '../../../../../components/Dao'
 import { Button } from '../../../../../components/Form'
 
 const basis = {
-    contaner: 'flex-wrap justify-between lg:flex-nowrap',
-    name: 'basis-full lg:basis-4/12 grow-0',
-    profile: 'basis-0 grow lg:basis-2/12 lg:grow-0',
-    wallet: 'basis-0 grow lg:basis-2/12 lg:grow-0',
-    allowance: 'basis-0 grow',
-    buttons: 'basis-full md:basis-0 md:grow-0',
+    contaner: 'flex items-center flex-wrap xl:flex-nowrap px-3 py-2 gap-x-6 gap-y-2',
+    name: 'basis-full lg:!basis-3/12',
+    profile: 'basis-5/12 md:basis-4/12 lg:!basis-[11.7%]',
+    wallet: 'basis-5/12 md:basis-4/12 lg:!basis-[11.7%]',
+    allowance: 'basis-full md:basis-4/12 lg:!basis-2/12',
+    balance: 'basis-full md:basis-4/12 lg:!basis-2/12',
+    buttons: 'basis-full md:basis-0 grow',
 }
 
 const ListItemSkeleton = () => {
@@ -38,16 +39,17 @@ const ListItemHeader = (props: React.HTMLAttributes<HTMLDivElement>) => {
 
     return (
         <div
-            className={classNames(
-                'flex items-center justify-between px-3 py-3 gap-x-4',
-                'text-xs text-gray-7c8db5',
-                className,
-            )}
+            className={classNames(basis.contaner, 'text-xs text-gray-7c8db5', className)}
         >
-            <div className="basis-auto md:grow lg:basis-4/12 lg:grow-0">name</div>
-            <div className={basis.profile}>profile</div>
-            <div className={basis.wallet}>wallet</div>
-            <div className={basis.allowance}>karma</div>
+            <div className={classNames('!basis-auto', basis.name)}>name</div>
+            <div className={classNames('!basis-auto', basis.profile)}>profile</div>
+            <div className={classNames('!basis-auto', basis.wallet)}>wallet</div>
+            <div className={classNames('!basis-auto', basis.allowance)}>karma</div>
+            <div
+                className={classNames('!basis-auto', basis.balance, 'whitespace-nowrap')}
+            >
+                token balance
+            </div>
             <div className={basis.buttons}></div>
         </div>
     )
@@ -76,13 +78,13 @@ const ListItem = (props: TListItemProps) => {
     }
 
     return (
-        <div
-            className={classNames(
-                'flex items-center px-3 py-2 gap-x-4 gap-y-2',
-                basis.contaner,
-            )}
-        >
-            <div className={basis.name}>
+        <div className={classNames(basis.contaner)}>
+            <div
+                className={classNames(
+                    basis.name,
+                    'overflow-hidden whitespace-nowrap text-ellipsis',
+                )}
+            >
                 <MemberIcon type="user" className="mr-2" size="sm" fixedWidth />
                 {item.username}
             </div>
@@ -90,25 +92,25 @@ const ListItem = (props: TListItemProps) => {
                 <CopyClipboard
                     className="font-light font-mono text-xs"
                     componentProps={{ text: item.profile.address }}
-                    label={shortString(item.profile.address, 6, 6)}
+                    label={shortString(item.profile.address, 5, 4)}
                 />
             </div>
             <div className={basis.wallet}>
                 <CopyClipboard
                     className="font-light font-mono text-xs"
                     componentProps={{ text: item.wallet.address }}
-                    label={shortString(item.wallet.address, 6, 6)}
+                    label={shortString(item.wallet.address, 5, 4)}
                 />
             </div>
             <div className={basis.allowance}>{item.allowance.toLocaleString()}</div>
-            <div className={basis.buttons}>
+            <div className={basis.balance}>{item.allowance.toLocaleString()}</div>
+            <div className={classNames(basis.buttons, 'text-end')}>
                 {member.isMember && (
                     <Button
                         type="button"
                         variant="outline-danger"
-                        size="sm"
                         className={classNames(
-                            'w-full md:w-auto md:!border-transparent md:disabled:!border-transparent',
+                            'w-full md:w-auto lg:!border-transparent lg:disabled:!border-transparent',
                         )}
                         onClick={() => onDelete(item.username)}
                         disabled={
@@ -117,7 +119,7 @@ const ListItem = (props: TListItemProps) => {
                         isLoading={item.isFetching}
                     >
                         <FontAwesomeIcon icon={faTimes} size="lg" />
-                        <span className="ml-2 md:hidden">Delete member</span>
+                        <span className="ml-2 lg:hidden">Delete member</span>
                     </Button>
                 )}
             </div>
