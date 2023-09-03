fb1e61b36d3b679dcb9e1ea8769356516afdd252
Alexander <alexander.s@gosh.sh>
2023-09-03 15:15:34 +0300

    Fix script

--- tests/upgrade_deploy_gosh.sh
+++ tests/upgrade_deploy_gosh.sh
@@ -3,27 +3,27 @@ set -e
 set -o pipefail
 
 case $1 in
-  1) VERSION="v1_x";;
-  2) VERSION="v2_x";;
-  3) VERSION="v3_x";;
-  4) VERSION="v4_x";;
-  5) VERSION="v5_x/v5.1.0";;
-  6) VERSION="v6_x/v6.0.0";;
-  *) VERSION="v${1}_x/v${1}.0.0";;
+  1) INPUT_VERSION="v1_x";;
+  2) INPUT_VERSION="v2_x";;
+  3) INPUT_VERSION="v3_x";;
+  4) INPUT_VERSION="v4_x";;
+  5) INPUT_VERSION="v5_x/v5.1.0";;
+  6) INPUT_VERSION="v6_x/v6.0.0";;
+  *) INPUT_VERSION="v${1}_x/v${1}.0.0";;
 esac
 
 case $2 in
-  1) VERSION2="v1_x";;
-  2) VERSION2="v2_x";;
-  3) VERSION2="v3_x";;
-  4) VERSION2="v4_x";;
-  5) VERSION2="v5_x/v5.1.0";;
-  6) VERSION2="v6_x/v6.1.0";;
-  *) VERSION2="v${2}_x/v${2}.0.0";;
+  1) INPUT_VERSION2="v1_x";;
+  2) INPUT_VERSION2="v2_x";;
+  3) INPUT_VERSION2="v3_x";;
+  4) INPUT_VERSION2="v4_x";;
+  5) INPUT_VERSION2="v5_x/v5.1.0";;
+  6) INPUT_VERSION2="v6_x/v6.1.0";;
+  *) INPUT_VERSION2="v${2}_x/v${2}.0.0";;
 esac
 
-echo "Version=$VERSION,Version2=$VERSION2"
+echo "Version=$INPUT_VERSION,Version2=$INPUT_VERSION2"
 
-./node_se_scripts/deploy.sh $VERSION $3
-. set-vars.sh $VERSION $3
-./upgrade_tests/set_up.sh $VERSION $VERSION2 $3
+./node_se_scripts/deploy.sh $INPUT_VERSION $3
+. set-vars.sh $INPUT_VERSION $3
+./upgrade_tests/set_up.sh $INPUT_VERSION $INPUT_VERSION2 $3
