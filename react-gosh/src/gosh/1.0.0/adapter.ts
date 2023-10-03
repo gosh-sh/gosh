50094334d6dff514b47ab6462ab0612cc09e4e94
roman.ec4a <rymka1989@gmail.com>
2023-10-03 22:59:16 +0300

    [web] - fix generate diff patch

--- react-gosh/src/gosh/1.0.0/adapter.ts
+++ react-gosh/src/gosh/1.0.0/adapter.ts
@@ -2783,7 +2783,9 @@ class GoshRepositoryAdapter implements IGoshRepositoryAdapter {
         original: string,
     ) => {
         // Get lib patch
-        let patch = Diff.createPatch(treepath, original, modified)
+        let patch = Diff.createPatch(treepath, original, modified, undefined, undefined, {
+            context: 0,
+        })
 
         // Format to GOSH patch
         patch = patch.split('\n').slice(4).join('\n')
