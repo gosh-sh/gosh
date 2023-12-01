60772dc37beccbf80870b8315598308693530ab3
roman.ec4a <rymka1989@gmail.com>
2023-12-01 05:28:19 +0300

    [contracts] - ackinacki deploy scripts

--- v6_x/v6.2.0/contracts/multisig/scripts/generate.sh
+++ v6_x/v6.2.0/contracts/multisig/scripts/generate.sh
@@ -11,13 +11,13 @@ GIVER_TVC="$GIVER_PATH/MultisigWallet.tvc"
 
 # Generate keys
 echo "========== Generate keys for GoshGiver"
-tonos-cli genphrase | grep -o '".*"' | tr -d '"' > $GIVER_PATH/$GIVER_SEED_FILE_OUT
+/opt/gosh/contracts/tonos-cli genphrase | grep -o '".*"' | tr -d '"' > $GIVER_PATH/$GIVER_SEED_FILE_OUT
 seed=`cat $GIVER_PATH/$GIVER_SEED_FILE_OUT`
-everdev signer add $SIGNER "$seed"
+# everdev signer add $SIGNER "$seed"
 echo $NETWORK > $GIVER_PATH/$GIVER_NETWORK_FILE_OUT
-everdev network add $NETWORK_NAME "$NETWORK"
+# everdev network add $NETWORK_NAME "$NETWORK"
 # Calculate GoshGiver address
 #GIVER_ADDR=$(everdev contract info $GIVER_ABI -s $SIGNER -n $NETWORK_NAME | sed -nr 's/Address:[[:space:]]+(.*)[[:space:]]+\(.*/\1/p')
-GIVER_ADDR=$(tonos-cli -j genaddr --abi $GIVER_ABI --setkey "$seed" --save $GIVER_TVC | jq -M ".raw_address" | cut -d '"' -f 2)
+GIVER_ADDR=$(/opt/gosh/contracts/tonos-cli -j genaddr --abi $GIVER_ABI --setkey "$seed" --save $GIVER_TVC | jq -M ".raw_address" | cut -d '"' -f 2)
 echo "========== GoshGiver address: $GIVER_ADDR"
 echo $GIVER_ADDR > $GIVER_PATH/Giver.addr
